13c634f11edfc1a1b5810e35bd70791a09c19c6a
Shoyeb45 <shoyebff45@gmail.com>
2024-01-24 23:34:41 +0530

    problems....

--- Codeforces/pleasantPairs.java
+++ Codeforces/pleasantPairs.java
@@ -4,30 +4,29 @@ public class pleasantPairs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        
-        int[] answer = {0, 0, 0};
 
-        for(int i=0 ; i<t ; i++){
+        int[] answer = { 0, 0, 0 };
+
+        for (int i = 0; i < t; i++) {
             int n = sc.nextInt();
 
             int[] arr = new int[n];
-            for(int j=0 ; j<n ; j++){
-                  arr[j] = sc.nextInt();
+            for (int j = 0; j < n; j++) {
+                arr[j] = sc.nextInt();
             }
 
-            for(int k=0 ; k<n ; k++){
-                for(int j=k+1 ; j<n ; j++){
-                    
-                    if(k < j && arr[k]*arr[j] == k+j+2){
-                     answer[i]+=1;
+            for (int k = 0; k < n; k++) {
+                for (int j = k + 1; j < n; j++) {
+
+                    if (k < j && (long) arr[k] * arr[j] == (long) k + j + 2) {
+                        answer[i] += 1;
                     }
                 }
-                }
             }
-        
+        }
 
-    for(int i=0 ;i<3 ; i++){
-        System.out.println(answer[i]);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(answer[i]);
         }
 
         sc.close();

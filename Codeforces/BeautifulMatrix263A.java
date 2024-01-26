55e1a41967aee3bef412d5012ef6f570e6888fd3
Shoyeb45 <shoyebff45@gmail.com>
2024-01-26 11:44:14 +0530

    Added some new problems

--- Codeforces/BeautifulMatrix263A.java
+++ Codeforces/BeautifulMatrix263A.java
@@ -6,113 +6,112 @@ public class BeautifulMatrix263A {
 
         int[][] matrix = new int[5][5];
 
-        for(int i=0 ;i<5 ; i++){
-            for(int j=0; j<5 ;j++){
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
                 matrix[i][j] = input.nextInt();
             }
         }
 
-        int count = 0 ;
+        int count = 0;
 
-        for(int i=0 ;i<5 ; i++){
-            for(int j=0; j<5 ;j++){
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
 
-                if(matrix[i][j] == 1){
-                    if(i == 2 && j == 2){
-                        count = 0 ;
-                    }else if(i>2 && j>2){
-                        for(int k=i ; k > 2 ;k--){
+                if (matrix[i][j] == 1) {
+                    if (i == 2 && j == 2) {
+                        count = 0;
+                    } else if (i > 2 && j > 2) {
+                        for (int k = i; k > 2; k--) {
                             count++;
                         }
-                        for(int k=j ; k > 2 ;k--){
+                        for (int k = j; k > 2; k--) {
                             count++;
                         }
                         // do {
-                        //     count++;
-                        //     --i;
+                        // count++;
+                        // --i;
                         // }while( i > 2);
                         // do {
-                        //     count++;
-                        //     --j;
+                        // count++;
+                        // --j;
                         // } while (j > 2);
-                    }else if(i<2 && j<2){
+                    } else if (i < 2 && j < 2) {
                         do {
                             count++;
                             ++i;
-                        } while (i != 2 );
-                        do {
-                            count++;
-                            ++j;
-                        } while (j != 2 );
-                    }else if(i>2 && j<2){
-                        for(int k=i ; k != 2 ;k--){
-                            count++;
-                        }
-
-                        // do {
-                        //     count++;
-                        //     --i;
-                        // } while (i > 2);
-                        
+                        } while (i != 2);
                         do {
                             count++;
                             ++j;
                         } while (j != 2);
-                    }else if(i == 2 ){
-                        if(j < 2){
+                    } else if (i > 2 && j < 2) {
+                        for (int k = i; k != 2; k--) {
+                            count++;
+                        }
+
+                        // do {
+                        // count++;
+                        // --i;
+                        // } while (i > 2);
+
                         do {
                             count++;
                             ++j;
-                        } while     (j != 2);
-                        }else{
-                            for(int k=j ; k != 2 ;k--){
+                        } while (j != 2);
+                    } else if (i == 2) {
+                        if (j < 2) {
+                            do {
+                                count++;
+                                ++j;
+                            } while (j != 2);
+                        } else {
+                            for (int k = j; k != 2; k--) {
                                 count++;
                             }
 
-                        // do {
-                        //     count++;
-                        //     --j;
-                        // } while (j > 2 );
+                            // do {
+                            // count++;
+                            // --j;
+                            // } while (j > 2 );
 
                         }
-                    }else if(j == 2 ){
-                        if(i < 2){
+                    } else if (j == 2) {
+                        if (i < 2) {
+                            do {
+                                count++;
+                                ++i;
+                            } while (i != 2);
+                        } else {
+                            for (int k = i; k != 2; k--) {
+                                count++;
+                            }
+
+                            // do {
+                            // count++;
+                            // --i;
+                            // } while (i > 2 );
+
+                        }
+                    } else {
                         do {
                             count++;
                             ++i;
                         } while (i != 2);
-                        }else{
-                            for(int k=i ; k != 2 ;k--){
-                                count++;
-                            }
-
-                        // do {
-                        //     count++;
-                        //     --i;
-                        // } while (i > 2 );
-
-                        }
-                    }else{
-                        do {
-                            count++;
-                            ++i;
-                        } while (i != 2);
-                        for(int k=j ; k != 2 ;k--){
+                        for (int k = j; k != 2; k--) {
                             count++;
                         }
                         // do {
-                        //     count++;
-                        //     --j;
+                        // count++;
+                        // --j;
                         // } while (j > 2);
                     }
-                     
-                   
+
                 }
             }
         }
 
-                System.out.print(count);
-            input.close();
-            }
-        }
-    
+        System.out.print(count);
+        input.close();
+    }
+}
+

f015fa37d265d009e15e8465d52be048e995e8fd
Shoyeb45 <shoyebff45@gmail.com>
2024-01-26 20:30:19 +0530

    helpful maths

--- Codeforces/helpfulMaths339A.java
+++ Codeforces/helpfulMaths339A.java
@@ -6,21 +6,24 @@ public class helpfulMaths339A {
        String sum = sc.nextLine();
 int[] num = new int[(sum.length()/2)+1];
        for(int i=0,j=0 ; i<sum.length() ; i+=2, j++){
-        num[j] = sum.charAt(i) - 48;
-       }
+            num[j] = sum.charAt(i) - 48;
+      }
 
-     for(int i=0; i<sum.length()/2 ; i++){
-        for(int j=i+1 ; i<sum.length()/2+1; j++){
+     for(int i=0; i<num.length-1 ; i++){
+        for(int j=i+1 ; j<num.length; j++){
             if(num[i] >= num[j]){
             int temp = num[i];
-            num[i] = num[i+1];
-            num[i+1] = temp;
+            num[i] = num[j];
+            num[j] = temp;
             }
         }
      }
 
-     for(int i=0 ; i<sum.length()/2+1; i++){
-        System.out.println(num[i]);
+     for(int i=0 ; i<num.length ; i++){
+        if(i == num.length - 1)
+        System.out.print(num[i]);
+        else
+        System.out.print(num[i]+"+");
      }
      sc.close();
     }

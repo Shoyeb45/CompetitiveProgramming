498f6f4f13d30c1781fb840208a9c98f8888f6e4
Shoyeb45 <shoyebff45@gmail.com>
2024-01-30 02:27:52 +0530

    don't know what's happening

--- Codeforces/collider.java
+++ Codeforces/collider.java
@@ -1,21 +1,38 @@
 import java.util.Scanner;
 
 public class collider {
-    public static int isRelaPrime(int a, int b){
+    public static boolean isRelaPrime(int a, int b){
         int hcf = 0;
         for(int i=1; i<=a|| i<=b ; i++){
-            if(a%i == 0 || b%i == 0){
+            if(a%i == 0 && b%i == 0){
                 hcf = i;
             }
         }
-        return hcf;
+        if(hcf == 1)
+        return true;
+        else
+        return false;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        System.out.println(isRelaPrime(n, m));
+        int[] reqNum = new int[m];
+        
+        for(int i=0; i<m ; i++){
+            String temp = sc.nextLine();
+            
+            // if(temp.charAt(0) == '+'){
+            //     reqNum[i] = temp.charAt(2);
+            // }else{
+            //     reqNum[i] = -temp.charAt(2);
+            // }
+            
+        }
+        for(int i =0; i<m ; i++){
+            System.out.println(reqNum[i]);
+        }
         sc.close();
 
     }

e730ed4d9264e4de5e181474931d31846cec813d
Shoyeb45 <shoyebff45@gmail.com>
2024-02-03 00:36:39 +0530

    Translation-easy

---
+++ Codeforces/chatRoom58A.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class chatRoom58A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
+    
+}

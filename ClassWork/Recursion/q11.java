a1beacace67f368c42a94026d93c21916e0da434
Shoyeb45 <shoyebff45@gmail.com>
2024-02-07 23:31:44 +0530

    q11

--- ClassWork/Recursion/q11.java
+++ ClassWork/Recursion/q11.java
@@ -3,9 +3,31 @@ package ClassWork.Recursion;
 import java.util.Scanner;
 
 public class q11 {
+    public static boolean isPalindrome(String s, int i, int strlen){
+        if(i == strlen/2 && strlen % 2 !=0 ){
+            return true;
+        }
+
+        if(i == strlen/2-1 && strlen % 2 ==0 && s.charAt(i) == s.charAt(i+1)){
+            return true;
+        }
+
+        if(strlen % 2 != 0){
+        if(s.charAt(i) == s.charAt(strlen-i-1)){
+            return true && isPalindrome(s, i+1, strlen);
+        }
+    }else{
+        if(s.charAt(i) == s.charAt(strlen-i-1)){
+            return true && isPalindrome(s, i+1, strlen);
+        }
+    }
+
+        return false;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
+        System.out.println(isPalindrome(s, 0, s.length()));
         sc.close();
     }
     

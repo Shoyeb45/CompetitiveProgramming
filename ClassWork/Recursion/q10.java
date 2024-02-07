a79d17d9a709186133b08d447fdce33ebbe7f973
Shoyeb45 <shoyebff45@gmail.com>
2024-02-07 23:09:17 +0530

    made function

--- ClassWork/Recursion/q10.java
+++ ClassWork/Recursion/q10.java
@@ -3,15 +3,19 @@ package ClassWork.Recursion;
 import java.util.Scanner;
 
 public class q10 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+    public static int strlen(String s){
         char[] a = s.toCharArray();
         int len = 0;
         for(char c : a){
             len++;
         }
-        System.out.println(len);
+        return len;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        
+        System.out.println(strlen(s));
         sc.close();
     }
 }
---
+++ ClassWork/Recursion/q11.java
@@ -0,0 +1,12 @@
+package ClassWork.Recursion;
+
+import java.util.Scanner;
+
+public class q11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        sc.close();
+    }
+    
+}

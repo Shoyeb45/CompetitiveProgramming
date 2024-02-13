e5eea38a176ce3eb0d31c2c01255cffe56835572
Shoyeb45 <shoyebff45@gmail.com>
2024-02-13 19:55:04 +0530

    incomplete question

---
+++ Codechef/alternatingWorkDays.java
@@ -0,0 +1,27 @@
+package Codechef;
+import java.util.*;
+public class alternatingWorkDays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int o=1; o<=t; o++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+
+            boolean ans = false;
+            for(int i = 1, j = 1; i<=(2 * q)/a || j <= 2 * ((9/a) - 1); i++, j++) {
+                if (  ) {
+                    ans = true;
+                    break;
+                } else {
+                    ans = false;
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
---
+++ Codechef/turnIt.java
@@ -0,0 +1,27 @@
+package Codechef;
+
+import java.util.*;
+
+public class turnIt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int p=1; p<=t; p++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int a = sc.nextInt();
+            int s = sc.nextInt();
+
+            if(u > v) {
+                if(( u * u - 2 * a * s) <= v * v)
+                System.out.println("Yes");
+                else
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+        }
+        sc.close();
+    }
+}

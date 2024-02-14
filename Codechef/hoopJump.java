84a5ce0a73ad71affac63f0253ca734863a96c9d
Shoyeb45 <shoyebff45@gmail.com>
2024-02-14 10:11:40 +0530

    two dishes

---
+++ Codechef/hoopJump.java
@@ -0,0 +1,16 @@
+package Codechef;
+
+import java.util.Scanner;
+
+public class hoopJump {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int p=1; p<=t; p++) {
+            int n = sc.nextInt();
+            System.out.println(n/2 + 1);       
+        }
+        sc.close();
+    } 
+}
---
+++ Codechef/theTwoDishes.java
@@ -0,0 +1,21 @@
+package Codechef;
+
+import java.util.Scanner;
+
+public class theTwoDishes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int p=1; p<=t; p++) {
+            int n = sc.nextInt();
+            int s = sc.nextInt();
+
+            if ( s <= n)  System.out.println(s);
+            else {
+                System.out.println(2*n - s);
+            }
+        }
+        sc.close();
+    }    
+}

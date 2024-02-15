d5cf9b44353d9467bc163aa4a17945946d81aebe
Shoyeb45 <shoyebff45@gmail.com>
2024-02-15 18:37:36 +0530

    merge sort algorithm

--- ClassWork/Array/mergeSort.java
+++ ClassWork/Array/mergeSort.java
@@ -3,20 +3,19 @@ package ClassWork.Array;
 import java.util.Scanner;
 
 public class mergeSort {
-    public static int[] merge(int[] a, int[] b, int n, int m) {
-        int k=0;
-        int[] c = new int[n+m];
-        for(int i=0, j=0; i < n || j < m; ) {
-            if(i == n) {
+    public static int[] merge(int[] a, int[] b) {
+        int[] c = new int[a.length + b.length];
+        int i = 0, j = 0, k = 0;
+        for(i = 0, j = 0 ; j < b.length || i < a.length; ) {
+            if(i == a.length) {
                 c[k] = b[j];
                 j++;
                 k++;
-            }
-            else if (j == m) {
+            } else if (j == b.length) {
                 c[k] = a[i];
                 i++;
                 k++;
-            } else if (a[i] <= a[j]) {
+            } else if (a[i] <= b[j]) {
                 c[k] = a[i];
                 i++;
                 k++;
@@ -27,23 +26,32 @@ public class mergeSort {
             }
         }
         return c;
-    }    
+    }
+
+    public static int[] sort(int[] a) {
+        if(a.length == 1) return a;
+
+        int len = a.length;
+        int[] tempA = new int[(len+1)/2];
+        int[] tempB = new int[len - (len+1)/2];
+
+        for(int i=0; i<=(len+1)/2-1; i++) tempA[i] = a[i];
+        for(int i=(len+1)/2,j = 0; i<=len-1; i++, j++) tempB[j] = a[i];
+
+        int[] sortedA = sort(tempA);
+        int[] sortedB = sort(tempB);
+        int[] Final =  merge(sortedA, sortedB);
+
+        return Final;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = sc.nextInt();
         int[] a = new int[n];
-        int[] b = new int[m];
 
-        for(int i=0; i<=n-1; i++)
-            a[i] = sc.nextInt();
-
-        for(int i=0; i<=m-1; i++)
-            b[i] = sc.nextInt();
-      
-        int[] c = merge(a, b, n, m);
-        for(int i=0; i<c.length; i++)
-        System.out.print(c[i]+" ");
+        for(int i=0; i<=n-1; i++)   a[i] = sc.nextInt();
+        int[] c = sort(a);
+        for(int i=0; i<c.length; i++)   System.out.print(c[i]+" ");
         
         sc.close();
 
--- Codeforces/makeEqual.java
+++ Codeforces/makeEqual.java
@@ -15,7 +15,6 @@ public class makeEqual {
         if(n != 1) {    
             int[] sorted = new int[n];
             Arrays.sort(sorted);
-            @SuppressWarnings("unused")
             int median = 0;
             if(n % 2 != 0) median = sorted[n/2];
             else           median = (sorted[n/2] + sorted[n/2 - 1])/2 ;

92b47b34eeffb500de979f0d24f08c60f73e24b4
Shoyeb45 <shoyebff45@gmail.com>
2024-02-21 17:59:29 +0530

    lower bound and upper bound

--- ClassWork/quesBinary5.java
+++ ClassWork/quesBinary5.java
@@ -1,19 +1,32 @@
 package ClassWork;
 // Searching upper bound insertion point of given number
- 
+
+import java.util.Scanner;
 
 public class quesBinary5 {
-    public static int freq(int[] a, int x) {
+    public static int upperBound(int[] a, int x) {
         int lower = 0;
         int upper = a.length - 1;
-
+        int UB = a.length;
         while (lower <= upper) {
             int mid = (lower + upper)/2;
-            int ans = 0;
-            
+            if(a[mid] > x) {
+                upper = mid - 1;
+            } else {
+                UB = mid+1;
+                lower = mid + 1;    
+            }
         }
+        return UB;
     }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<=n-1; i++) arr[i] = sc.nextInt();
+        int x = sc.nextInt();
+
+        System.out.println(upperBound(arr, x));
+        sc.close();
     }
 }

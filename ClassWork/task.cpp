282671ba95bc48a24e0609d8104c3735557c908a
Shoyeb45 <shoyebff45@gmail.com>
2024-02-22 11:13:40 +0530

    maximum of minimum of tasks

---
+++ ClassWork/task.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> p;
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        int temp;
+        cin >> temp;
+        p.push_back(temp);
+    }
+
+
+    while (low <= high)
+    {
+        if(isSubsequ(s, t));
+    }
+    
+
+
+}

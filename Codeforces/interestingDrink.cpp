21d19ff5dccf29e5607af50a468bbdaa5fc6cb2e
Shoyeb45 <shoyebff45@gmail.com>
2024-02-23 17:28:10 +0530

    interesting drink

--- Codeforces/interestingDrink.cpp
+++ Codeforces/interestingDrink.cpp
@@ -1,25 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int numberShop(int x[], int q, int n) {
-    int low = 0;
-    int high = n - 1;
-    int count = 0;
-    while (low <= high) 
-    {
-        int mid = (low + high)/2;
-        if(mid >= 0 && q <= x[mid-1]) {
-            count++;
-            high = mid + 1;
-        } else if(q <= x[mid] ) {
-            count++;
-            high = mid - 1;
-        } else {
-            high = mid - 1;
+int upperBound(int a[], int x, int n) {
+        int lower = 0;
+        int upper = n - 1;
+        int UB = 0;
+        while (lower <= upper) {
+            int mid = (lower + upper)/2;
+            if(a[mid] > x) {
+                upper = mid - 1;
+            } else {
+                UB = mid+1;
+                lower = mid + 1;    
+            }
         }
+        return UB;
     }
-    return count;
-}
 
 int main() {
     int n, q;
@@ -33,7 +29,7 @@ int main() {
     for(int i=0; i<q; i++)  {
         int y;
         cin >> y;
-        int cnt = numberShop(x, y, n);
+        int cnt = upperBound(x, y, n);
         cout << cnt << "\n";   
     }
 

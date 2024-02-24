bdd2a474b42385f733b2b20307dce2c95d595fef
Shoyeb45 <shoyebff45@gmail.com>
2024-02-24 08:35:56 +0530

    rank list

--- Codeforces/rankList.cpp
+++ Codeforces/rankList.cpp
@@ -14,6 +14,11 @@ int main() {
 
     sort(vp.begin(), vp.end());
     reverse(vp.begin(), vp.end());
+
+    cout <<"\n";
+
+    for(int i=0; i<n; i++) cout << vp[i].first << " " << vp[i].second << '\n';
+    
     for(int i=0; i<n-1; i++) {
         if(vp[i].first == vp[i+1].first && vp[i].second > vp[i+1].second) {
             int temp = vp[i].second;
@@ -21,7 +26,19 @@ int main() {
             vp[i+1].second = temp;
         }
     }
-    
+    int ind = k- 1;
+    int cnt = 1;
+    for(int i=0; i<n; i++) {
+        if(i != ind) {
+            if(vp[i].first == vp[ind].first && vp[i].second == vp[ind].second) {
+                cnt++;
+            }
+        }
+    }
+    cout <<"\n";
 
+    for(int i=0; i<n; i++) cout << vp[i].first << " " << vp[i].second << '\n';
+
+    cout << cnt;
     return 0;
 }

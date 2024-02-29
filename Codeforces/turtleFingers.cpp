4923f7913abb411bed52a0ee71354220e6026f07
Shoyeb45 <shoyebff45@gmail.com>
2024-02-29 19:00:59 +0530

    turtle fingers

---
+++ Codeforces/turtleFingers.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        long long a, b, l;
+        cin >> a >> b >> l;
+
+        for(int x=0; ; x++) {
+            for(int y=0; ; y++) {
+                if(exp((double)(a), (double)(x)))
+            }
+        }
+    }
+    
+}

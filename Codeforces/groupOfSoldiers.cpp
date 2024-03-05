d2d6ada952bfd78b0c73642d43b67e71035f300c
Shoyeb45 <shoyebff45@gmail.com>
2024-03-05 08:46:27 +0530

    group of soldier - unsolved

---
+++ Codeforces/groupOfSoldiers.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ll n, m;
+    cin >> n >> m;
+    ll a[n];
+    for(ll i=0; i<n; i++)   cin >> a[i];     
+
+    ll b[m];
+    for(ll i=0; i<m; i++)   cin >> b[i];    
+     
+    return 0;
+}

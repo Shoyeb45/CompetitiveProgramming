c2552d6c8350632bb80a34dc105a570548b56c58
Shoyeb45 <shoyebff45@gmail.com>
2024-03-06 22:36:25 +0530

    2d prefix sum

---
+++ CPP For CP/2dPrefixSum.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int a[n][m];
+    int l1, r1, l2, r2;
+    cin >> l1 >> r2 >> l2 >> r2;
+    int prefixRowSum[n][m];
+    
+    return 0;
+}

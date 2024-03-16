c4485ef9c0c0ed17c8ec616c5c8e150ff2e4ef18
Shoyeb45 <shoyebff45@gmail.com>
2024-03-16 08:35:30 +0530

    ambitious kid - easy

---
+++ Codeforces/TLE-31_Sheet/800/halloumiBoxes.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isAsce(vector<int> &a) {
+    bool ans = true;
+    for(int i = 0 ; i < a.size() - 1; i++) {
+        if(a[i] >= a[i+1])   ans = ans && true;
+        else    ans = false;
+    }
+    return ans;
+}
+
+bool isDesc(vector<int> &a) {
+    bool ans = true;
+    for(int i = 0 ; i < a.size() - 1; i++) {
+        if(a[i] <= a[i+1])   ans = ans && true;
+        else    ans = false;
+    }
+    return ans;
+}
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> a(n);
+        for(int i = 0 ; i < n; i++) cin >> a[i];
+
+        if(isDesc(a))   cout << "YES\n";
+        else if(isAsce(a) && k != 1)    cout << "YES\n";
+        else if ( k == 1 )   cout << "NO\n";
+        else    cout << "YES\n";
+    }
+    
+}

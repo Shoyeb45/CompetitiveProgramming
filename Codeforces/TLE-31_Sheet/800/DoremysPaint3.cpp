12ec2a78bc50fec199b4e05ded85df745f59c7a9
Shoyeb45 <shoyebff45@gmail.com>
2024-03-22 15:22:48 +0530

    Doremy's paint 3 - solved

--- Codeforces/TLE-31_Sheet/800/DoremysPaint3.cpp
+++ Codeforces/TLE-31_Sheet/800/DoremysPaint3.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int absolute(int x, int y) {
+    if (x - y >= 0) return x - y; 
+
+    return -1 * (x - y);
+}
+
 int main() {
     int t;
     cin >> t;
@@ -9,29 +15,46 @@ int main() {
     {
         int n;
         cin >> n;
-        vector<int> a(n);
+        vector<int> a;
+        set<int> s;
+        for(int i = 0; i < n ; i++){
+            int temp;
+            cin >> temp;
+            a.push_back(temp);
+            s.insert(temp);
+            }
 
-        for(int i = 0; i < n ; i++) cin >> a[i];
 
-        sort(a.begin(), a.end());
+        if(s.size() == 1)   cout << "YES\n";
+        else if (s.size() > 2)  cout << "NO\n";
+        else {
+            sort(a.begin(), a.end());
 
-        if(n % 2 == 1) {
             int cnt1 = 1, cnt2 = 1;
             bool count1 = true;
             for(int i = 0; i < n - 1 ; i++) {
                 if(count1) {
                     if(a[i] == a[i+1])  cnt1++;   
-                    else    count1 = false;
-                } else {
-                    if(a[i] == a[i+1])  cnt2++;   
+                        else    count1 = false;
+                    } else {
+                        if(a[i] == a[i+1])  cnt2++;
+                    }
                 }
+            if(absolute(cnt1, cnt2) <= 1)   cout << "YES\n";
+            else    cout << "NO\n";
+
             }
 
-            if((cnt1 == n/2 && cnt2 == n - n/2) || (cnt2 == n/2 && cnt1 == n - n/2))    cout << "YES\n";
-            else    cout << "NO\n";
-        } else {
-            
-        }
+
+        // if(n % 2 == 1) {
+        //     if((cnt1 == n/2 && cnt2 == n - n/2) || (cnt2 == n/2 && cnt1 == n - n/2) || (cnt1 == n))    cout << "YES\n";
+        //     else    cout << "NO\n";
+        // } else {
+        //     if(cnt1 == cnt2 || cnt1 == n)    cout << "YES\n";
+        //     else    cout << "NO\n";
+        // }
+
+
     }
     
 }

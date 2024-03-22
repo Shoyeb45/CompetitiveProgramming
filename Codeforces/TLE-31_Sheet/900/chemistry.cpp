65c0df82ae59a6ff8af2005e53ec186e1ec77949
Shoyeb45 <shoyebff45@gmail.com>
2024-03-22 12:50:15 +0530

    chemistry - incomplete

---
+++ Codeforces/TLE-31_Sheet/900/chemistry.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+
+        if(n- k == 1)   cout << "YES\n"
+        else {
+            
+        }
+    }
+    
+}

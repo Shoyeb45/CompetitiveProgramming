744ec0d14f477f5999ea67c5f33a81fe04f27505
Shoyeb45 <shoyebff45@gmail.com>
2024-03-26 18:52:17 +0530

    vaslije in cacak

--- Codeforces/TLE-31_Sheet/900/vasilijeInCacak.cpp
+++ Codeforces/TLE-31_Sheet/900/vasilijeInCacak.cpp
@@ -7,9 +7,15 @@ int main() {
 
     while (t--)
     {
-        int n, k, x;
+        long long n, k, x;
         cin >> n >> k >> x;
         
+        long long totalSum = (n) * (n + 1) / 2;
+        long long sum1 = k * (k+1) /2;
+        long long sum2 = totalSum - ((n - k) * (n - k + 1) / 2);
+
+        if(x >= sum1 && x <= sum2)  cout << "YES\n";
+        else    cout <<"NO\n";
     }
     
 }

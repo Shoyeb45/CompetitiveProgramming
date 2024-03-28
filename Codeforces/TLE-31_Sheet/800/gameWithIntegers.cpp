7181cc77c3420b5dcee7e8535dcdd7a1c5f5f684
Shoyeb45 <shoyebff45@gmail.com>
2024-03-28 18:21:22 +0530

    Game With Integers

--- Codeforces/TLE-31_Sheet/800/gameWithIntegers.cpp
+++ Codeforces/TLE-31_Sheet/800/gameWithIntegers.cpp
@@ -9,7 +9,7 @@ int main() {
     {
         int n;
         cin >> n;
-        if(n % 3 == 1)  cout << "First\n";
+        if((n + 1) % 3 == 0 || (n - 1) % 3 == 0)  cout << "First\n";
         else    cout << "Second\n";
     }
     

38e90fb047628a5c7700a9e6021e090756e9e2eb
Shoyeb45 <shoyebff45@gmail.com>
2024-03-28 07:13:11 +0530

    goals of victory

---
+++ Codeforces/TLE-31_Sheet/800/goalsOfVictory.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n - 1);
+
+        for(int i = 0 ; i < n - 1; i++) cin >> a[i];
+        
+    }
+    
+}

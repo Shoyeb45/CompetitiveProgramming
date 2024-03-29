0780a371047ddc9c1c53cf7a093d566174cdaf81
Shoyeb45 <shoyebff45@gmail.com>
2024-03-29 09:45:45 +0530

    ambitious kid

--- Codeforces/TLE-31_Sheet/800/ambitiousKid.java
+++ Codeforces/TLE-31_Sheet/800/ambitiousKid.java
@@ -4,18 +4,19 @@ public class ambitiousKid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] a = new int[n];
+        
         int smallestPos = Integer.MAX_VALUE;
 
         for(int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-            a[i] = Math.abs(a[i]);
-            if(a[i] >= 0 && a[i] <= smallestPos) {
-                smallestPos = a[i];
+            int temp = sc.nextInt();
+            temp = Math.abs(temp);
+            if(temp <= smallestPos) {
+                smallestPos = temp;
             }
             
         }
 
         System.out.println(smallestPos);
+        sc.close();
     }
 }

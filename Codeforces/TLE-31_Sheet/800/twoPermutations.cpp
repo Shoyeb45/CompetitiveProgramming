2cf15846c537c3cdee4bc3b0f0f0b647f33e4eca
Shoyeb45 <shoyebff45@gmail.com>
2024-04-03 17:06:33 +0530

    Two permutation

---
+++ Codeforces/TLE-31_Sheet/800/twoPermutations.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+typedef long long ll;
+
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, a, b;
+        cin >> n >> a >> b;
+        
+        
+    }
+    
+}

9156a3786c98b2518f6e38d24072c032a42be348
Shoyeb45 <shoyebff45@gmail.com>
2024-04-04 22:49:30 +0530

    queue

---
+++ CPP For CP/queue.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Declaration 
+    queue<int> q;
+    // We can only access frist element and insert at last position
+
+    // push - insert at last position
+    q.push(3);   // {3}
+    q.push(5);   // {3, 5}
+    q.push(10);  // {3, 5, 10}
+
+    // front - access first element
+    cout << q.front() << endl;
+
+    // Pop - remove element from the front
+    q.pop(); // {5, 10}
+    cout << q.front() << endl;
+
+    // size 
+    cout << q.size() << "\n";
+
+    q.pop(); // {10}
+    q.pop();
+
+    // Empty - return true
+    if(q.empty())   cout << "Queue is empty.\n";
+}

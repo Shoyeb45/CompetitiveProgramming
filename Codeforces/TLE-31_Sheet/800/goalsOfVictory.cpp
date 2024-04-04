e19c40fd95d7bff261d4e58712c7816174420460
Shoyeb45 <shoyebff45@gmail.com>
2024-04-04 10:43:55 +0530

    Goals of victory

--- Codeforces/TLE-31_Sheet/800/goalsOfVictory.cpp
+++ Codeforces/TLE-31_Sheet/800/goalsOfVictory.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+typedef long long ll;
 
 int main() {
     int t;
@@ -10,9 +12,14 @@ int main() {
         int n;
         cin >> n;
         vector<int> a(n - 1);
+        ll sum = 0 ;
 
-        for(int i = 0 ; i < n - 1; i++) cin >> a[i];
-        
+        for(int i = 0 ; i < n - 1; i++) {
+            cin >> a[i];
+            sum += a[i];
+        }
+
+        cout << -1 * sum << endl;
     }
     
 }

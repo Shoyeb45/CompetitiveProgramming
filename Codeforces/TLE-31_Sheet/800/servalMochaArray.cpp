338c7163cda2b5475a5f1922b72fbe0350722ddb
Shoyeb45 <shoyebff45@gmail.com>
2024-04-04 12:42:19 +0530

    Serval and Mocha's Array

--- Codeforces/TLE-31_Sheet/800/servalMochaArray.cpp
+++ Codeforces/TLE-31_Sheet/800/servalMochaArray.cpp
@@ -13,7 +13,20 @@ int main() {
         cin >> n;
         vector<int> a(n);
         loop(i, n)  cin >> a[i];
+        int ans = false;
         
+        loop(i, n ) {
+            for(int j = i + 1; j < n; j++) {
+                if(__gcd(a[i], a[j]) <= 2) {
+                    ans = true;
+                    break;
+                }
+                
+            }
+        }
+
+        if(ans) cout << "YES\n";
+        else    cout << "NO\n";
     }
     
 }

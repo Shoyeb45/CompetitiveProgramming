08853da43de32349aa81e103cf96dbc2af6dd416
Shoyeb45 <shoyebff45@gmail.com>
2024-04-06 20:06:45 +0530

    Make it divisible by 25

--- Codeforces/TLE-31_Sheet/900/makeItDivisible25.cpp
+++ Codeforces/TLE-31_Sheet/900/makeItDivisible25.cpp
@@ -2,60 +2,75 @@
 #define loop(i, n) for(int i = 0; i < n; i++)  
 using namespace std;
 typedef long long ll;
+ 
 
-bool compareSecond(const pair<int,int>& a, const pair<int,int>& b) {
-    return a.second < b.second;
-}
-
-void solve2(ll n) {
+void solve(ll n) {
     string s = to_string(n);
-    vector<pair<int,int>> vp;
-
-    loop(i, s.size() - 1) {
+    int ans = INT_MAX;
+    int t = s.size();
+    loop(i, s.size()) {
         for(int j = i + 1; j < s.size(); j++) {
             int check = (s[i] - '0') * 10 + (s[j] - '0');
             if(check % 25 == 0) {
-                vp.push_back(make_pair(i, j));
+                ans = min(ans, (j - i - 1) + (t - 1 - j));
             }
         }
     }
-    sort(vp.begin(), vp.end(), compareSecond);
-    int ans = INT_MAX;
-    loop(i, vp.size()) {
-        int step = (vp[i].second - vp[i].first - 1) + (s.size() - vp[i].second - 1);
-        ans = min(ans, step);
-    }
+
     cout << ans << endl;
-    return;
 }
 
-void solve1(ll n) {
-    string s = to_string(n);
-    int ind0 = 0, ind5 = 0;
+// void solve2(ll n) {
+//     string s = to_string(n);
+//     // vector<pair<int,int>> vp;
 
-    loop(i, s.size()) {
-        if(i >= 1 && s[i] == '0' && s[i-1] == '0')   ind0 = max(ind0, i);
-        if(s[i] == '5') {
-            if(i >= 1) {
-                int x = s[i - 1] - '0';
-                int check = x * 10 + (s[i] - '0');
-                ind5 = max(ind5, check % 25 == 0? i: 0);
-            }
-            if(i <= s.size() - 2) {
-                ind5 = max(ind5, s[i + 1] == '0' ? i + 1: 0);
-            }
-        }
-    }
-    int ans = max(ind0, ind5);
+//     int ans = INT_MAX;
+//     loop(i, s.size() - 1) {
+//         for(int j = i + 1; j < s.size(); j++) {
 
-    if(ans == 0) {
-        solve2(n);
-        return;
-    }
+//             int check = (s[i] - '0') * 10 + (s[j] - '0');
+//             if(check % 25 == 0) {
+//                 ans = min(ans,  (j - i - 1) + (s.size() - i - 1));
+//                 // vp.push_back(make_pair(i, j));
+//             }
+//         }
+//     }
+    // sort(vp.begin(), vp.end(), compareSecond);
+    // loop(i, vp.size()) {
+    //     int step = (vp[i].second - vp[i].first - 1) + (s.size() - vp[i].second - 1);
+    //     ans = min(ans, step);
+    // }
+//     cout << ans << endl;
+//     return;
+// }
 
-    cout << s.size() - 1 - ans << endl;
+// void solve1(ll n) {
+//     string s = to_string(n);
+//     int ind0 = 0, ind5 = 0;
+
+//     loop(i, s.size()) {
+//         if(i >= 1 && s[i] == '0' && s[i-1] == '0')   ind0 = max(ind0, i);
+//         if(s[i] == '5') {
+//             if(i >= 1) {
+//                 int x = s[i - 1] - '0';
+//                 int check = x * 10 + (s[i] - '0');
+//                 ind5 = max(ind5, check % 25 == 0? i: 0);
+//             }
+//             if(i <= s.size() - 2) {
+//                 ind5 = max(ind5, s[i + 1] == '0' ? i + 1: 0);
+//             }
+//         }
+//     }
+//     int ans = max(ind0, ind5);
+
+//     if(ans == 0) {
+//         solve2(n);
+//         return;
+//     }
+
+//     cout << s.size() - 1 - ans << endl;
     
-}
+// }
 
 
 int main() {
@@ -69,7 +84,7 @@ int main() {
 
         if(n % 25 == 0)  cout << 0 << "\n";
         else {
-            solve1(n);
+            solve(n);
         }
     }
 }

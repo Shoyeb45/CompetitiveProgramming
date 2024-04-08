89c4fdc0a13c3e1f9a14e1bc8be934edbc00afab
Shoyeb45 <shoyebff45@gmail.com>
2024-04-08 09:34:32 +0530

    colck conversion

--- Codeforces/clockConversion.cpp
+++ Codeforces/clockConversion.cpp
@@ -7,7 +7,39 @@ int main() {
     cin >> t;
     while (t--)
     {
-            
+        string s;
+        cin >> s;
+        int hour  = (s[0] - '0') * 10 + (s[1] - '0');
+        int min = (s[3] - '0') * 10 + (s[4] - '0');
+        string time = "";
+        string hr;
+        string minute;
+        if(hour >= 1 && hour < 12) {
+            time = "AM";
+        } else if(hour == 0){
+            time = "AM";
+            hour = 12;
+        } else if(hour == 12) {
+            time = "PM";
+            hr = "12";
+        } else {
+            hour = hour - 12;
+            time = "PM";
+        }
+            if(!(hour/10)) { 
+                hr = "0";
+                hr += to_string(hour);
+            } else {
+                hr = to_string(hour);
+            }
+
+        if(!(min/10)) {
+            minute = "0";
+            minute += to_string(min);
+        } else {
+            minute = to_string(min);
+        }
+        cout << hr + ":" + minute + " " + time << endl;
     }
     
 }

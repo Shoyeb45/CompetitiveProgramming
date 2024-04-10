4670a0dc4234c02eb8dbab6dd107ddb8448f4258
Shoyeb45 <shoyebff45@gmail.com>
2024-04-10 16:35:26 +0530

    forked

--- Codeforces/TLE-31_Sheet/900/forked.cpp
+++ Codeforces/TLE-31_Sheet/900/forked.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+#define loop(i, n)  for(int i = 0 ; i < n; i++)
 int main() {
     int t;
     cin >> t;
@@ -12,6 +12,26 @@ int main() {
         cin >> xk >> yk;
         cin >> xq >> yq;
         
+        set<pair<int, int>> vp;
+
+        vp.insert({xk + a, yk + b});
+        vp.insert({xk - a, yk + b});
+        vp.insert({xk + b, yk + a});
+        vp.insert({xk - b, yk + a});
+        vp.insert({xk + b, yk - a});
+        vp.insert({xk - b, yk - a});
+        vp.insert({xk + a, yk - b});
+        vp.insert({xk - a, yk - b});
+
+        int ans = 0;
+        for(auto i : vp) {
+            int x = i.first;
+            int y = i.second;
+            if((x + a == xq && y + b == yq) || (x - a == xq && y - b == yq) || (x + b == xq && y + a == yq) || (x - b == xq && y - a == yq) || (x + b == xq && y - a == yq) || (x - b == xq && y + a == yq) || (x + a == xq && y - b == yq) || (x - a == xq && y + b == yq))    
+                ans++;
+        }
+
+        cout << ans << "\n";
     }
     
 }

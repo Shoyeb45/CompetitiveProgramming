a350abee65122f3463ebb8dfacff3e53d8010cce
Shoyeb45 <shoyebff45@gmail.com>
2024-04-14 22:00:57 +0530

    prime Query

--- Codeforces/scalar contest/primeQueries.cpp
+++ Codeforces/scalar contest/primeQueries.cpp
@@ -12,9 +12,6 @@ int main() {
     // Array for 2 to root(R)
     vector<bool> isPrime(n + 1, true);
     isPrime[0] = isPrime[1] = false;
-    for (int i = 2; i <= n; i++) {
-        isPrime[i] = true;
-    }
 
     // Checking prime between 2 to root(R)
     for (ll i = 2; (ll)(i * i) <= n; i++) {
@@ -30,16 +27,17 @@ int main() {
     primeBetween[0] = 0;
     for (int i = 2; i <= n; i++) {
         if (isPrime[i]) {
-            for (long long j = max((L + i - 1) / i * i, (long long) (i * i)); j <= R; j += i) {
+            ll strt =   max((L + i - 1) / i * i, (long long) (i * i));
+            for (long long j = strt; j <= R; j += i) {
                 primeBetween[j - L] = false;
             }
         }
     }
 
     vector<ll> p;
-    for(ll i = 1; i < R - L + 1 ; i++) {
+    for(ll i = 1; i < R - L + 1 ; i++) 
         if(primeBetween[i]) p.push_back(i + 1);
-    }
+    
     
     int check = p[k-1];
 
@@ -47,24 +45,10 @@ int main() {
     int sumPrime = 0;
     int sumNonPrime = 0;
     int temp = check;
-    // sieve
-
-    int MAX = 10;
-        bool prime[MAX + 1];
-        for(int i = 0; i <= MAX; i++)    prime[i] = true;
-        prime[0] = 0;
-        prime[1] = 0;
-        
-        for(long long i = 2; (i * i) <= MAX; i++) {
-            if(prime[i]) {
-                for(long long j = i * i; j <= MAX; j += i) {
-                    prime[j] = 0;
-                }
-            }
-        }
+    
     while(temp >= 1) {
         int d = temp % 10;
-        if(prime[d])    sumPrime++;
+        if(isPrime[d])    sumPrime++;
         else sumNonPrime++;
         temp /= 10;
     }

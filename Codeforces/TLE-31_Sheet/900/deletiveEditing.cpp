9e7601b962f703dae14d33ea6636017d2e3d4fe9
Shoyeb45 <shoyebff45@gmail.com>
2024-04-15 20:48:14 +0530

    Deletive editing

---
+++ Codeforces/TLE-31_Sheet/900/deletiveEditing.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+typedef long long ll;
+
+void solve() {
+    string s, t;
+    cin >> s >> t;
+    
+    if(s == t) {
+        cout << "YES\n";
+        return;
+    }
+    
+}
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        solve();
+    }
+}

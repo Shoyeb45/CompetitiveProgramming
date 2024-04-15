6ebe10c0da74aa273d7e58c5d7346bb4201a522c
Shoyeb45 <shoyebff45@gmail.com>
2024-04-15 20:32:31 +0530

    Make it Increasing

--- Codeforces/TLE-31_Sheet/900/makeItIncreasing.cpp
+++ Codeforces/TLE-31_Sheet/900/makeItIncreasing.cpp
@@ -3,15 +3,6 @@
 using namespace std;
 typedef long long ll;
 
-bool isAsce(vector<int> &a) {
-    bool ans = true;
-    for(int i = 0 ; i < a.size() - 1; i++) {
-        if(a[i] > a[i+1])   ans = ans && true;
-        else    ans = false;
-    }
-    return ans;
-}
-
 bool isDesc(vector<int> &a) {
     bool ans = true;
     for(int i = 0 ; i < a.size() - 1; i++) {
@@ -29,13 +20,27 @@ int main() {
     {
         int n;
         cin >> n;
+
         vector<int> a(n);
         loop(i, n)  cin >> a[i];
 
-        if(isDesc(a))   cout << 0 << "\n";
-        else if(isAsce(a))  cout << -1 << "\n";
-        else {
-            
+        int ans = 0, check = 1;
+
+        for(int i = n - 2; i >= 0; i--) {
+            if(a[i + 1] != 0 && a[i + 1] <= a[i]) {
+                int cnt = log2(a[i]/a[i + 1]) + 1;
+                int div = pow(2, cnt);
+                a[i] /= div;
+                ans += cnt;
+            } 
+            if(a[i + 1] == 0)   {
+                check = 0;
+                break;
+            }
         }
+
+        if(check)   cout << ans << "\n";
+        else    cout << -1 << "\n";
     }
+
 }

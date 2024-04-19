2a42a8bdcae9adeca137c544005eb5ef2a6e03c7
Shoyeb45 <shoyebff45@gmail.com>
2024-04-19 20:09:37 +0530

    Practice Sheet

--- ClassWork/PracticeSheet/34StringSearch.py
+++ ClassWork/PracticeSheet/34StringSearch.py
@@ -1,3 +1,12 @@
-l = list(map(int, input().split()))
+l = list(map(str,input().split()))
 s = input()
+def checkString(l, s):
+    for str in l:
+        if str == s:
+            print("String present")
+            return
+    
+    print("String not present")
 
+checkString(l, s)
+    

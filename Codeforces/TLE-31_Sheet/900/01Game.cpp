12d84cb2e2460bb19f82ee0150cf1c1cdad92392
Shoyeb45 <shoyebff45@gmail.com>
2024-04-28 22:56:14 +0530

    01 game

--- Codeforces/TLE-31_Sheet/900/01Game.cpp
+++ Codeforces/TLE-31_Sheet/900/01Game.cpp
@@ -2,21 +2,34 @@
 using namespace std;
 #define loop(i, n) for(int i = 0; i < n; i++)
 typedef long long ll;
-
+const ll mod = 1000000007;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t;
     cin >> t;
-    while(t--) {
+
+    while (t--) {
         string s;
         cin >> s;
 
-        int count1 = 0;
-        loop(i, s.size())   count1 += (s[i] == '1');
+        int ones = 0;
+        for(char ch: s) {
+            ones += (ch == '1');
+        }
+        int zeroes = s.size() - ones;
 
-        if(count1 == s.size() || count1 == 0)    cout << "NET\n";
-        else{
-
+        if(ones == 0 || zeroes == 0) {
+            cout << "NET\n";
+        } else {
+            int ans = min(ones, zeroes);
+            if(ans % 2 == 1)
+                cout << "DA\n";
+            else
+                cout << "NET\n";
         }
     }
+    
+    return 0;
 }

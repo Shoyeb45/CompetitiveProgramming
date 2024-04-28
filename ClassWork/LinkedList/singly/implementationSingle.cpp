f81d63befec760e749e396bd297d7fe2c3ce209e
Shoyeb45 <shoyebff45@gmail.com>
2024-04-28 16:28:43 +0530

    Linked list - deletion and updation

--- ClassWork/LinkedList/singly/implementationSingle.cpp
+++ ClassWork/LinkedList/singly/implementationSingle.cpp
@@ -52,6 +52,53 @@ void add(Node* &head, int k, int val) {
     temp->next = newNode;
 }
 
+// Updating the value of node at kth position
+void change(Node* head, int k, int val) {
+    Node* temp = head;
+    int pos = 0;
+    while(pos != k) {
+        temp = temp->next;
+        pos++;
+    }
+    temp->x = val;
+}
+
+// Deleting the first node - at head
+void delHead(Node* &head) {
+    Node* temp = head;
+    head = head->next;
+    free(temp);
+}
+
+// Delete at the end of the linked list - at tail
+void delTail(Node* &head) {
+    Node* temp = head;
+    while (temp->next->next != NULL) {
+        temp = temp->next;
+    }
+    Node* temp2 = temp->next;
+    temp->next = NULL;
+    free(temp2);
+}
+
+// Deleting at kth position
+void del(Node* &head, int k) {
+    if(k == 0) {
+        delHead(head);
+        return;
+    }
+    Node* temp = head;
+    int pos = 0;
+    while(pos != k - 1) {
+        temp = temp->next;
+        pos++;    
+    }
+    
+    Node* temp2 = temp->next;
+    temp->next = temp->next->next;
+    free(temp2);
+}
+
 // Function for traversing through linked list 
 void printLL(Node* head) {
     Node* temp = head;
@@ -71,16 +118,43 @@ int main() {
     // cout << a->x << " " << a->next << endl;
     // cout << b->x << " " << b->next << endl;
 
-    // Inserting values
+    // Inserting values at head
     insert(a, 12);
     printLL(a);
     insert(a, 13);
     printLL(a);
     insert(a, 32);
     printLL(a);
+
+    // Inserting at end position 
     insertEnd(a, 21);
     printLL(a);
+
+    // Inserting at arbitary position 
     add(a, 1, 10);
+    printLL(a);
+    change(a, 3, 99);
+    printLL(a);
+
+    // deleting at head
+    delHead(a);
+    printLL(a);
+    delHead(a);
+    printLL(a);
+
+    // delete at end
+    delTail(a);
+    printf("Deleting at tail:\n");
+    printLL(a);
+
+    // Inserting some values
+    insert(a, 9);
+    insertEnd(a, 11);
+    printLL(a);
+
+    // Deleting at arbitary position
+    del(a, 2);
+    cout << "\nDeleting at position 2\n";
     printLL(a);
     return 0;
 }

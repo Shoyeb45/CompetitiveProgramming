c9628e5cc0f81a301a49d7a94cf0c1ada314af58
Shoyeb45 <shoyebff45@gmail.com>
2024-05-02 09:20:12 +0530

    Inserting element in sorted array

--- ClassWork/Array/practice1.java
+++ ClassWork/Array/practice1.java
@@ -8,29 +8,28 @@ public class practice1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n+1];
-        for(int i = 0 ; i<=n-1; i++){
+        int[] arr = new int[n + 1];
+        for (int i = 0; i <= n - 1; i++) {
             arr[i] = sc.nextInt();
         }
         int m = sc.nextInt();
 
-        for(int i = 0 ; i<=n; i++){
-            if(arr[i] > m){
-                for(int j = n-1; j>=i; j--){
-                    arr[j+1] = arr[j];
+        for (int i = 0; i <= n; i++) {
+            if (arr[i] > m) {
+                for (int j = n - 1; j >= i; j--) {
+                    arr[j + 1] = arr[j];
                 }
                 arr[i] = m;
-                break; 
+                break;
             }
         }
 
-        for(int i = 0 ; i<=n; i++){
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i <= n; i++) {
+            System.out.print(arr[i] + " ");
         }
-        
 
         sc.close();
 
     }
-    
+
 }

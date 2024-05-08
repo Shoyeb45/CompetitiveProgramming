ff57388a529a9392f2455f0ebb1db1e1a35f017e
Shoyeb45 <shoyebff45@gmail.com>
2024-05-08 23:29:27 +0530

    Yet Another Broken Keyboard

---
+++ Codeforces/Practice/1200/yetAnotherBrokenKeyboard.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+typedef long long ll;
+const ll mod = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, k;
+    cin >> n >> k;
+    string s; cin >> s;
+    set<char> typableChar;
+    loop(i, k) {
+        char x; cin >> x;
+        s.insert(x);
+    }
+
+    ll ans = 0;
+    if(k == 26) {
+        ans = 1LL * n * 1LL * (n + 1)/2;
+    } else {
+            int cnt = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(typableChar.find(s[i]) == typableChar.end()) {
+                ans += 1LL * cnt * 1LL * (cnt + 1)/2;
+                cnt = 0;
+            } else {
+                cnt++;
+            }
+        }
+        ans += 1LL * cnt * 1LL * (cnt + 1)/2;
+    }
+
+    cout << ans << '\n';
+    return 0;
+}

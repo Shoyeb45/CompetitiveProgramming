8bde78972f11071c750f6c8f6165fb5025b746c6
Shoyeb45 <shoyebff45@gmail.com>
2024-05-11 23:22:39 +0530

    array Merging

--- Codeforces/TLE-31_Sheet/1000/arrayMerging.cpp
+++ Codeforces/TLE-31_Sheet/1000/arrayMerging.cpp
@@ -5,25 +5,37 @@ typedef long long ll;
 const ll mod = 1000000007;
 
 void solve() {
-    int n;
-    cin >> n;
-    map<int, int> m;
-    loop(i, n) {
-        int a;
-        cin >> a;
-        m[a]++;
-    }
+    int n; cin >> n;
+    vector<int> a(n + 1), b(n + 1);
+    for(int i = 1; i <= n; i++)  cin >> a[i];
+    for(int i = 1; i <= n; i++)  cin >> b[i];
+    
+    vector<int> occurence_a(2 *n + 1);
+    vector<int> occurence_b(2 *n + 1);
+    int start = 1;
 
-    loop(i, n) {
-        int a;
-        cin >> a;
-        m[a]++;
+    for(int i = 2; i <= n; i++) {
+        if(a[i] != a[i - 1]) {
+            occurence_a[a[i - 1]] = max(occurence_a[a[i - 1]], i - start);
+            start = i;
+        }
     }
+    occurence_a[a[n]] = max(occurence_a[a[n]], n - start + 1);
+
+    start  = 1;
+    for(int i = 2; i <= n; i++) {
+        if(b[i] != b[i - 1]) {
+            occurence_b[b[i - 1]] = max(occurence_b[b[i - 1]], i - start);
+            start = i;
+        }
+    }
+    occurence_b[b[n]] = max(occurence_b[b[n]], n - start + 1);
+
     int ans = 0;
-    for(auto it: m) {
-        ans = max(ans, it.second);
+    for(int i = 1; i <= 2*n; i++) {
+        ans = max(ans, occurence_a[i] + occurence_b[i]);
     }
-    cout << ans << endl;
+    cout << ans << '\n';
 }
 int main() {
     ios_base::sync_with_stdio(false);

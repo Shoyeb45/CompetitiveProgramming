d3eac72c87c7e3029864d4a1edfe19e528383395
Shoyeb45 <shoyebff45@gmail.com>
2024-05-31 00:20:04 +0530

    Aggressive Cows

--- Spoj/aggressiveCows.cpp
+++ Spoj/aggressiveCows.cpp
@@ -4,6 +4,17 @@ using namespace std;
 typedef long long ll;
 const ll mod = 1000000007;
 
+bool check(vector<int> &a, int c, ll mid) {
+    int cnt = 1;
+    int last_cow = a[0];
+    for(int  i = 1; i < a.size(); i++) {
+        if(a[i] - last_cow >= mid) {
+            cnt++;
+            last_cow = a[i];
+        }
+    }
+    return cnt >= c;
+}
 void solve() {
     int n, c;
     cin >> n >> c;
@@ -14,13 +25,25 @@ void solve() {
     sort(l.begin(), l.end());
 
     ll ans = 0;
-    int low = 0, high = 
+    ll low = 0, high = 1e9;
+
+    while(low <= high) {
+        ll mid = low + (high - low)/2;
+        // cout << mid << '\n';
+        if(check(l, c, mid)) {
+            ans = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    cout << ans << '\n';
 }
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int tt;
-    tt = 1
+    tt = 1;
     cin >> tt;
 
     while (tt--) {

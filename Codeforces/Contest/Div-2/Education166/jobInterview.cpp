5e3b5e1a676144c34ffef5104e8b39c7a637b283
Shoyeb45 <shoyebff45@gmail.com>
2024-05-30 23:44:56 +0530

    Job Interview - Incomplete

---
+++ Codeforces/Contest/Div-2/Education166/jobInterview.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+typedef long long ll;
+const ll mod = 1000000007;
+
+void solve() {
+    int n, m;
+    vector<int> p(n + m + 1), t(n + m + 1);
+    loop(i, n + m + 1) 
+        cin >> p[i];
+
+    loop(i, n + m + 1) 
+        cin >> t[i];
+
+    
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tt;
+    tt = 1
+    cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}

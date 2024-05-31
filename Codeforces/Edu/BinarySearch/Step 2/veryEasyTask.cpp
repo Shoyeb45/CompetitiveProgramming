066e6e12dda187ddbbaf426056e91d23ee323ee1
Shoyeb45 <shoyebff45@gmail.com>
2024-05-31 12:33:17 +0530

    Very Easy Task

--- Codeforces/Edu/BinarySearch/Step 2/veryEasyTask.cpp
+++ Codeforces/Edu/BinarySearch/Step 2/veryEasyTask.cpp
@@ -4,28 +4,31 @@ using namespace std;
 typedef long long ll;
 const ll mod = 1000000007;
 
-bool check(int mid, int x, int y, int n) {
-    if(mid < min(x, y))
-        return false;
-    int time = mid - min(x, y);
-    int cnt = 1;
-    int rem = 0;
-    while(time >= max(x, y)) {
-        cnt += (1 + max(x, y)/min(x, y));
-        time -= max(x, y);
-        rem += (max(x, y) % min(x, y));
-    }
+bool check(ll mid, ll x, ll y, ll n) {
+    // if(mid < min(x, y))
+    //     return false;
+    // ll time = mid - min(x, y);
+    // ll cnt = 1;
+    // ll rem = 0;
+    // while(time >= max(x, y)) {
+    //     cnt += (1 + max(x, y)/min(x, y));
+    //     time -= max(x, y);
+    //     rem += (max(x, y) % min(x, y));
+    // }
 
-    cnt += (rem/min(x, y));
-    return cnt >= n;
+    // cnt += (rem/min(x, y));
+    // cnt += (time/min(x, y));
+    // return cnt >= n;
+    ll t1 = mid/x, t2 = mid/y;
+    return (1 + t1 + t2) >= n;
 }
 void solve() {
-    int n, x, y;
+    ll n, x, y;
     cin >> n >> x >> y;
-    int low = 1, high = max(x, y) * n;
-    int ans = 0;
+    ll low = 0, high = 2e9;
+    ll ans = 1;
     while(low <= high) {
-        int mid = low + (high - low)/2;
+        ll mid = low + (high - low)/2;
         if(check(mid, x, y, n)) {
             ans = mid;
             high = mid - 1;
@@ -33,7 +36,7 @@ void solve() {
             low = mid + 1;
         }
     }
-    cout << ans << '\n';
+    cout << ans + min(x, y) << '\n';
 }
 int main() {
     ios_base::sync_with_stdio(false);

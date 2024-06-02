13af0189a57f749118a4cfb3918634e8ad190544
Shoyeb45 <shoyebff45@gmail.com>
2024-06-02 12:34:32 +0530

    Equation

--- Codeforces/Edu/BinarySearch/Step 2/childrenHoliday.cpp
+++ Codeforces/Edu/BinarySearch/Step 2/childrenHoliday.cpp
@@ -5,7 +5,16 @@ typedef long long ll;
 const ll mod = 1000000007;
 
 void solve() {
+    int m, n;
+    cin >> m >> n;
+    vector<vector<int>> a(n, vector<int> (3, 0));
+    loop(i, n) {
+        loop(i, 3) {
+            cin >> a[i];
+        }
+    }
 
+    
 }
 int main() {
     ios_base::sync_with_stdio(false);
---
+++ Codeforces/Edu/BinarySearch/Step 2/equation.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+typedef long long ll;
+const ll mod = 1000000007;
+
+bool check(double x, double c) {
+    double cmp = x * x + sqrt(x);
+    // cout << "Cmpr: " << cmp << '\n';
+    return (cmp <= c);
+}
+void solve() {
+    double c;
+    cin >> c;
+
+    double low = 0, high = 1e10;
+    double ans = 0;
+    int iteration = 1000;
+    while(iteration--) {
+        double mid = low + (high - low)/2;
+        if(check(mid, c)) {
+            ans = mid;
+            low = mid + 1;
+        // cout << "MID: " << mid << '\n';
+        } else {
+            high = mid - 1;
+        }
+    }
+    cout << fixed << setprecision(15) << ans << '\n';
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}

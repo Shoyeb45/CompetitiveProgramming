4f39d5516c78f5fdf9a9f530225534fca8154840
Shoyeb45 <shoyebff45@gmail.com>
2024-06-07 16:59:51 +0530

    Student Council

--- Codeforces/Edu/BinarySearch/Step 2/studentCouncil.cpp
+++ Codeforces/Edu/BinarySearch/Step 2/studentCouncil.cpp
@@ -3,15 +3,34 @@ using namespace std;
 #define loop(i, n) for(int i = 0; i < n; i++)
 typedef long long ll;
 const ll mod = 1000000007;
+ll n, k;
+vector<ll> a;
+
+bool check(ll mid) {
+    ll tot = 0;
+    for(auto x: a) {
+        tot += min(x, mid);
+    }
+    return tot >= k * mid;
+}
 
 void solve() {
-    int k, n;
     cin >> k >> n;
-    vector<int> a(n);
-    loop(i, n) {
+    a.resize(n);
+    loop(i, n)
         cin >> a[i];
+    ll low = 0, high = 1e11, ans = 0;
+
+    while(low <= high) {
+        ll mid = (low + high)/2;
+        if(check(mid)) {
+            ans = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
     }
-    
+    cout << ans << '\n';
 }
 int main() {
     ios_base::sync_with_stdio(false);

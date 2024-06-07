fac99da464165244f7a653ef852088f8bfec993e
Shoyeb45 <shoyebff45@gmail.com>
2024-06-07 17:00:30 +0530

    XOR Sequence

--- Codeforces/Contest/Div-2/951/xorSequence.cpp
+++ Codeforces/Contest/Div-2/951/xorSequence.cpp
@@ -7,14 +7,60 @@ const ll mod = 1000000007;
 void solve() {
     int x, y;
     cin >> x >> y;
-    for(int i = 20; i <= 40; i++) {
-        cout << ((i) ^ x) << "  ";
+    ll k = 0;
+    vector<int> bits_x;
+    vector<int> bits_y;
+    while(x && y) {
+        // bits_x.push_back(x % 2);
+        // bits_y.push_back(y % 2);
+        if(x % 2 == y % 2)
+            k++;
+        else    
+            break;
+        x /= 2;
+        y /= 2;
     }
-    cout << '\n';
-    for(int i = 20; i <= 40; i++) {
-        cout << ((i) ^ y) << "  ";
+    if(!x) {
+        while(y) {
+            if(y % 2 == 0) {
+                k++;
+            } else {
+                break;
+            }
+            y /= 2;
+        }
     }
-    cout << '\n';
+    if(!y) {
+        while(x) {
+            if(x % 2 == 0) {
+                k++;
+            } else {
+                break;
+            }
+            x /= 2;
+        }
+    }
+    // reverse(bits_x.begin(), bits_x.end());
+    // reverse(bits_y.begin(), bits_y.end());
+    // cout << "BITS_X \n" ;
+    // loop(i, bits_x.size())
+    //     cout << bits_x[i] << ' ';
+    // cout<<'\n';
+    // cout << "BITS_Y \n" ;
+    // loop(i, bits_y.size())
+    //     cout << bits_y[i] << ' ';
+    // cout<<'\n';
+    // int i = 0;
+    // while(i < min(bits_x.size(), bits_y.size())) {
+    //     if(bits_x[i] == bits_y[i])
+    //         k++;
+    //     else
+    //         break;
+    //     i++;
+    // }
+    ll ans = pow(2, k);
+    // cout << "k: " << k <<'\n';
+    cout << ans << '\n';
 }
 int main() {
     ios_base::sync_with_stdio(false);

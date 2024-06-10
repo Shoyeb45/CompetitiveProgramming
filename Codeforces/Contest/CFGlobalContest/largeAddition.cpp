63123c4bb3ef52cc8b56971f8c1c8467031231ec
Shoyeb45 <shoyebff45@gmail.com>
2024-06-10 23:04:12 +0530

    Large addition 😪

--- Codeforces/Contest/CFGlobalContest/largeAddition.cpp
+++ Codeforces/Contest/CFGlobalContest/largeAddition.cpp
@@ -12,63 +12,22 @@ auto init = []() {
     return 'c';
 }();
 
+bool is_zero(string s) {
+    loop(i, s.size() - 1) {
+        if(s[i] == '0')
+            return false;
+    }
+    return true;
+}
+
 void solve() {
     string s;
     cin >> s;
-
-    if(s.back() == '9') {
+    bool flag1 = is_zero(s);
+    if(s[0] == '1' && s.back() != '9' && flag1) 
+        cout << "YES\n";
+    else
         cout << "NO\n";
-        return;
-    }
-    for(int i = 0; i < s.size() - 1; i++) {
-        if(s[i] == '0') {
-            cout << "NO\n";
-            return;
-        }
-    }
-
-    int carry = 0;
-    int x = (s[0] - '0') * 10 + (s[1] - '0');
-    // cout << x << "\n\n";
-    for(int i = s.size() - 1; i >= 0; i--) {
-        int curr = (s[i] - '0');
-        int ans = 0;
-        // cout << "Curr " << curr << '\n';
-        // cout << "carry " << carry << '\n';
-        for(int i = 5; i <= 9; i++) {
-                bool check = 0;
-            for(int j = 5; j <= 9; j++) {
-                int temp = i + j;
-                if(((temp + carry) % 10) == (curr)) {
-                    // cout << "i : " << i << " j: " << j << '\n';
-                    ans = temp;
-
-                    carry = temp/10;
-                    check = 1;
-                    break;
-                }
-            }
-            if(check)
-                break;
-        }
-        // cout << "digit: " << (int)(log10(ans)) + 1  << '\n';
-        // cout << "ans: " << ans << "\n\n";
-        if(i == 0) {
-            if(((int)(log10(ans)) + 1 )== 2) {
-                cout << "NO\n";
-                return;
-            } 
-        }
-        if(!ans) {
-            cout << "NO\n";
-            return;
-        }
-        if(ans == x) {
-            cout << "YES\n";
-            return;
-        }
-    }
-    cout << "YES\n";
     return;
 }
 

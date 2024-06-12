85b4050ed4485b5104f12a1006c4fd7c3af7a960
Shoyeb45 <shoyebff45@gmail.com>
2024-06-12 10:49:34 +0530

    creating words

---
+++ Codeforces/Contest/Div-4/954/creatingWords.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+typedef long long ll;
+const ll mod = 1000000007;
+
+auto init = []() { 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+void solve() {
+    string a, b;
+    cin >> a >> b;
+    
+    swap(a[0], b[0]);
+    cout << a << " " << b << '\n';
+}
+
+int main() {
+    int tt;
+    tt = 1;
+    cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}

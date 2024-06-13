dc10704d916e603120d926be57622b080543db64
Shoyeb45 <shoyebff45@gmail.com>
2024-06-13 11:59:22 +0530

    Good prefixes - solution 2

--- Codeforces/Contest/Div-4/954/goodPrefixes.cpp
+++ Codeforces/Contest/Div-4/954/goodPrefixes.cpp
@@ -14,22 +14,42 @@ auto init = []() {
     return 'c';
 }();
 
+// Solution - 1
+
+// void solve() {
+//     cin >> n;
+//     a.resize(n);
+//     map<ll, ll> m;
+
+//     loop(i, n) {
+//         cin >> a[i];
+//     }
+    
+//     ll sum = 0, ans = 0;
+//     loop(i , n) {
+//         m[2 * a[i]]++;
+//         sum += a[i];
+//         if(m.find(sum) != m.end()) {
+//             ans++;
+//         }
+//     }
+//     cout << ans << '\n';
+// }
+
+// Solution - 2
+
 void solve() {
     cin >> n;
-    a.resize(n);
-    map<ll, ll> m;
-
+    ll ans = 0;
+    ll mx = INT_MIN, sum = 0;
     loop(i, n) {
-        cin >> a[i];
-    }
-    
-    ll sum = 0, ans = 0;
-    loop(i , n) {
-        m[2 * a[i]]++;
-        sum += a[i];
-        if(m.find(sum) != m.end()) {
+        ll x;
+        cin >> x;
+
+        mx = max(x, mx);
+        sum += x;
+        if(sum == 2 * mx) 
             ans++;
-        }
     }
     cout << ans << '\n';
 }

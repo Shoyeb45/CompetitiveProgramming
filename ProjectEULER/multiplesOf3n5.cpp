056ae25b93084a903492b369422e64957cdfe0e1
Shoyeb45 <shoyebff45@gmail.com>
2024-06-15 15:30:06 +0530

    Multiple of 3 and 5

---
+++ ProjectEULER/multiplesOf3n5.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+#define range(x) (x).begin(), (x).end()
+typedef long long ll;
+const ll mod = 1000000007;
+
+auto init = []() { 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+void solve() {
+    int sum = 0;
+    for(int i = 1; i < 1000; i++) {
+        if(i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    cout << sum ;
+}
+
+int main() {
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}

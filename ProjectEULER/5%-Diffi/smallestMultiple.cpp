5219832f6f8e51d81ca83414a4b9847e5eba61c0
Shoyeb45 <shoyebff45@gmail.com>
2024-06-19 00:57:28 +0530

    smallest multiple

---
+++ ProjectEULER/5%-Diffi/smallestMultiple.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+#define range(x) (x).begin(), (x).end()
+typedef long long ll;
+const ll mod = 1000000007;
+
+auto init = []() { 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+// void solve() {
+
+    // Brute force solution - but worked due to higher initial value
+
+    // for(ll i = 252000; ; i++) {
+    //     bool check = 0;
+    //     for(int j = 1; j <= 20; j++) {
+    //         if(i % j == 0) {
+    //             check = 1;
+    //         } else {
+    //             check = 0;
+    //             break;
+    //         }
+    //     }
+    //         // cout << i << '\n';
+    //     if(check) {
+    //         cout << i << '\n';
+    //         break;
+    //     }
+    // }
+
+// }
+
+
+void solve() {
+
+}
+
+int main() {
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}

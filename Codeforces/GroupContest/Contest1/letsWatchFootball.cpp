b19d0da1553ade5997326826653db3b716a1e0d5
Shoyeb45 <shoyebff45@gmail.com>
2024-06-18 19:24:02 +0530

    Let's watch football

---
+++ Codeforces/GroupContest/Contest1/letsWatchFootball.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+#define range(x) (x).begin(), (x).end()
+typedef long long ll;
+const ll mod = 1000000007;
+
+auto init = []() { 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int a, b, c;
+
+void solve() {
+    cin >> a >> b >> c;
+    cout << ((a * c - c * b) + (b - 1))/b << '\n';     
+}
+
+int main() {
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}

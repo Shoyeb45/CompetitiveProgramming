2d96a212d1fa6728cd8a67783e2b466f7c17b416
Shoyeb45 <shoyebff45@gmail.com>
2024-06-18 21:30:02 +0530

    Finally solved!! - Watermelon 🌟😁😪

--- Codeforces/TLE-31_Sheet/800/watermelon.cpp
+++ Codeforces/TLE-31_Sheet/800/watermelon.cpp
@@ -1,1 +1,43 @@
-x
+#include<bits/stdc++.h>
+
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+#define range(x) (x).begin(), (x).end()
+typedef long long ll;
+const ll mod = 1000000007;
+
+auto init = []() { 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int n;
+
+void solve() {
+    cin >> n;
+
+    if(n % 2 == 1) {
+        cout << "NO\n";
+        return;
+    }  
+
+    if(n == 2) {
+        cout << "NO\n";
+        return;
+    }
+    cout << "YES\n";
+}
+
+int main() {
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}

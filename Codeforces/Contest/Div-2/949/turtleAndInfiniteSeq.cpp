81f3cf432060a82abbbff44abf3531055d288f31
Shoyeb45 <shoyebff45@gmail.com>
2024-06-22 14:30:47 +0530

    Turtle and infinite sequence

--- Codeforces/Contest/Div-2/949/turtleAndInfiniteSeq.cpp
+++ Codeforces/Contest/Div-2/949/turtleAndInfiniteSeq.cpp
@@ -1,17 +1,42 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 #define loop(i, n) for(int i = 0; i < n; i++)
+#define range(x) (x).begin(), (x).end()
 typedef long long ll;
 const ll mod = 1000000007;
 
-void solve() {
-    ll n, m;
-    cin >> n >> m;
-    
+auto init = []() { 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+bool is_set_bit(ll num, int pos) {
+    return ((num >> pos) & 1) == 1;
 }
+int n, m;
+void solve() {
+    cin >> n >> m;
+    int l = max(0, n - m);
+    int r = n + m;
+
+    ll val = 1;
+    ll ans = 0;
+    for(int i = 0; i <= 32; i++) {
+        if(r - l >= val) 
+            ans += val;
+        else if(is_set_bit(l, i)) 
+            ans += val;
+        else if(is_set_bit(r, i))
+            ans += val;
+        val *= 2;
+    }
+    cout << ans << '\n';
+}
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     int tt;
     tt = 1;
     cin >> tt;
---
+++ Codeforces/Practice/800/aliceAndBooks.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+#define range(x) (x).begin(), (x).end()
+typedef long long ll;
+const ll mod = 1000000007;
+
+auto init = []() { 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int n;
+vector<int> a;
+
+void solve() {
+    cin >> n;
+    a.resize(n);
+    int idx = 0;
+
+    loop(i, n) {
+        cin >> a[i];
+    }    
+}
+
+int main() {
+    int tt;
+    tt = 1;
+    cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}

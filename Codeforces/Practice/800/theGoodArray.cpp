b2778698e4b5569abe8007211033385918a9fb3f
Shoyeb45 <shoyebff45@gmail.com>
2024-06-27 22:58:26 +0530

    The good array

---
+++ Codeforces/Practice/800/theGoodArray.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+#define range(x) (x).begin(), (x).end()
+typedef long long ll;
+const ll mod = 1000000007;
+
+auto init = []() { 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int n, k;
+void solve() {
+    cin >> n >> k;
+    cout << 2 + (n - 2) / k << '\n'; 
+}
+
+int main() {
+    int tt;
+    tt = 1;
+    cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}

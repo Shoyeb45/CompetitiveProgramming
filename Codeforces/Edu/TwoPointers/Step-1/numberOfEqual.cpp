15a40b5848b25578b88bd4eda72e82ec68449e2f
Shoyeb45 <shoyebff45@gmail.com>
2024-07-04 20:06:23 +0530

    Number of Equal: Two pointers approach - infnite loop

--- Codeforces/Edu/TwoPointers/Step-1/numberOfEqual.cpp
+++ Codeforces/Edu/TwoPointers/Step-1/numberOfEqual.cpp
@@ -15,7 +15,8 @@ auto init = []() {
 
 int n, m;
 void solveBS() {
-    // Binary Search Solution
+    // Binary Search Solution 
+    // TC : O(n * log(m))
     cin >> n >> m;
     vector<int> a(n), b(m);
     loop(i, n)
@@ -30,13 +31,60 @@ void solveBS() {
     cout << ans << '\n';
 }
 
+void solveTP() {
+    // Two pointers solution
+
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    loop(i, n)
+        cin >> a[i];
+    loop(j, m) 
+        cin >> b[j];
+
+    int i = 0, j = 0;
+    ll ans = 0;
+    while(i < n && j < m) {
+        if(a[i] == b[j]) {
+            ll num1 = 1, num2 = 1;
+            while(i < n - 1 && a[i] == a[i + 1]) {
+                i++;
+                num1++;
+            }
+            while(j < m - 1 && b[j] == b[j + 1]) {
+                j++;
+                num2++;
+            }
+            ans += 1LL * num1 * num2;
+            i++;
+            j++;
+        } else if(a[i] > a[j]) {
+            while(j < m) {
+                if(b[j] > a[i]) 
+                    break;
+                if(b[j] == a[i])
+                    break;
+                j++;
+            }
+        } else {
+            while(i < n) {
+                if(a[i] > b[j])
+                    break;
+                if(b[j] == a[i])
+                    break;
+                i++;
+            }
+        }
+    }
+    cout << ans << '\n';
+}
+
 int main() {
     int tt;
     tt = 1;
     // cin >> tt;
 
     while (tt--) {
-        solveBS();
+        solveTP();
     }
     
     return 0;

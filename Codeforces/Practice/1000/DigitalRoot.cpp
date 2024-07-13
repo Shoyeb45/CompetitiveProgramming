0020e1dc59f17f89f8a59cb038a5642111e45aac
Shoyeb45 <shoyebff45@gmail.com>
2024-07-14 01:04:47 +0530

    Digital Root

---
+++ Codeforces/Practice/1000/DigitalRoot.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define loop(i, n) for(int i = 0; i < n; i++)
+#define range(x) (x).begin(), (x).end()
+typedef long long ll;
+const ll mod = 1000000007;
+
+auto init = []() { 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+ll k, x;
+void solve() {
+    cin >> k >> x;
+    cout << x + (k - 1) * 9 * 1LL << '\n';
+}
+
+int main() {
+    int tt;
+    tt = 1;
+    cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}

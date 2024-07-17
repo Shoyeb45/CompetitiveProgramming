702f5a0ac9c33c53489afdf16c59df0942152845
Shoyeb45 <shoyebff45@gmail.com>
2024-07-17 11:47:13 +0530

    Raising Bacteria

---
+++ Codeforces/Practice/1000/RaisingBacteria.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define loop(i, n) for(int i = 0; i < n; i++)
+#define range(x) (x).begin(), (x).end()
+typedef long long ll;
+const ll mod = 1000000007;
+
+auto init = []() { 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+ll x;
+void solve() {
+    cin >> x;
+    
+    bitset<64> a(x);
+    cout << a.count() << '\n';
+}
+
+int main() {
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}

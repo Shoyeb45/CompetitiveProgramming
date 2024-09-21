cc7b1ce67956cb7066638b6d3ac3190469cd92e2
Shoyeb45 <shoyebff45@gmail.com>
2024-09-21 09:07:49 +0530

    Scuza

--- Codeforces/Practice/1200/Scuza.cpp
+++ Codeforces/Practice/1200/Scuza.cpp
@@ -6,15 +6,15 @@ typedef long long ll;
 const ll mod = 1000000007;
 
 int n, q;
-vector<pair<ll, int>> a;
-int lowerBound(ll k) {
-    int low = 0, high = n - 1;
-    int ans = n;
+vector<ll> a;
+int lowerBound(vector<ll> &a, ll k) {
+    int low = 1, high = n;
+    int ans = n + 1;
     while(low <= high) {
         int mid = low + (high - low) / 2;
 
-        if(a[mid].first > k) {
-            ans = a[mid].second;
+        if(a[mid] > k) {
+            ans = mid;
             high = mid - 1;
         }
         else {
@@ -29,23 +29,23 @@ void solve() {
     a.resize(n);
 
     for(int i = 0; i < n; i++) {
-        cin >> a[i].first;
-        a[i].second = i;
+        cin >> a[i];
     }
 
     vector<ll> prefix(n + 1, 0);
+    vector<ll> prefixMax(n + 1, 0);
 
     for(int i = 1; i <= n; i++) {
-        prefix[i] += prefix[i - 1] + a[i - 1].first;
+        prefix[i] += prefix[i - 1] + a[i - 1];
+        prefixMax[i] = max(prefixMax[i - 1], a[i - 1]);
     }
-
-    sort(range(a));
     
     while(q--) {
         ll k;
         cin >> k;
-        int ind = lowerBound(k);
-        cout << prefix[ind] << ' ';
+      
+        int ind = lowerBound(prefixMax, k);
+        cout << prefix[ind - 1] << " ";
     }
     cout << "\n";
 }

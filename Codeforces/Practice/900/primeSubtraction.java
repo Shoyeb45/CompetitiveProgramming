a39c90cce12ed6fa3ecdf16bf59bd0122229b4ad
Shoyeb45 <shoyebff45@gmail.com>
2024-09-27 23:04:42 +0530

    prime Subtraction

--- Codeforces/Practice/900/primeSubtraction.java
+++ Codeforces/Practice/900/primeSubtraction.java
@@ -5,21 +5,20 @@ public class primeSubtraction {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
 
-        for(int i=1; i<=k ; i++){
+        for (int i = 1; i <= k; i++) {
             long a = sc.nextLong();
             long b = sc.nextLong();
 
-            if(a % 2 == 0 && b % 2 == 0){
+            if (a % 2 == 0 && b % 2 == 0) {
                 System.out.println("YES");
-            }else if((a % 2 == 1 && b % 2 == 0) || (a % 2 == 0 && b % 2 == 1)){
-                if(a - b >= 2)
-                System.out.println("YES");
+            } else if ((a % 2 == 1 && b % 2 == 0) || (a % 2 == 0 && b % 2 == 1)) {
+                if (a - b >= 2)
+                    System.out.println("YES");
                 else
-                System.out.println("NO");
-            }else{
+                    System.out.println("NO");
+            } else {
                 System.out.println("YES");
             }
-
 
         }
         sc.close();

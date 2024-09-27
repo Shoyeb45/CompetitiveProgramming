b8f3b05564691c9adbc0c4a8a5086ed80d762e68
Shoyeb45 <shoyebff45@gmail.com>
2024-09-27 22:59:19 +0530

    Petya and String

--- Codeforces/Practice/800/petyaString.java
+++ Codeforces/Practice/800/petyaString.java
@@ -5,26 +5,25 @@ public class petyaString {
         Scanner sc = new Scanner(System.in);
         String gift1 = sc.nextLine();
         String gift2 = sc.nextLine();
-boolean ans = false;
-        for(int i=0 ; i<gift1.length() ; i++){
-            if(gift1.toLowerCase().charAt(i) == gift2.toLowerCase().charAt(i) ){
-             ans = true;
-            }else if(gift1.toLowerCase().charAt(i) < gift2.toLowerCase().charAt(i)){
+        boolean ans = false;
+        for (int i = 0; i < gift1.length(); i++) {
+            if (gift1.toLowerCase().charAt(i) == gift2.toLowerCase().charAt(i)) {
+                ans = true;
+            } else if (gift1.toLowerCase().charAt(i) < gift2.toLowerCase().charAt(i)) {
                 System.out.println(-1);
                 ans = false;
                 break;
-            }else{
+            } else {
                 System.out.println(1);
                 ans = false;
                 break;
             }
         }
 
-        if(ans)
-        System.out.println(0);
+        if (ans)
+            System.out.println(0);
 
         sc.close();
-        
-        
+
     }
 }

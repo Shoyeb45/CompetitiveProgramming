332c0de416435c01042008810841840af640a1bd
Shoyeb45 <shoyebff45@gmail.com>
2024-09-28 22:42:30 +0530

    Erase First Or Second Letter

---
+++ Codeforces/TLE-31_Sheet/1100/EraseFirstOrSecondLetter.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+const ll mod = 1000000007;
+
+int n;
+string s;
+void solve() {
+    cin >> n >> s;
+    set<char> st;
+
+    ll ans = 0;
+    for(char ch: s) {
+        st.insert(ch);
+        ans += st.size();
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    NFS
+    int tt;
+    tt = 1;
+    cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}

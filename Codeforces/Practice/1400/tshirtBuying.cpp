8b94b77b4006bc4693a3df12b6f5570bd34a497f
Shoyeb45 <shoyebff45@gmail.com>
2024-10-03 18:12:10 +0530

    T shirt Buying

--- Codeforces/Practice/1400/tshirtBuying.cpp
+++ Codeforces/Practice/1400/tshirtBuying.cpp
@@ -1,24 +1,64 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define loop(i, n) for(int i = 0; i < n; i++)
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
 typedef long long ll;
+const ll mod = 1000000007;
 
-int main() {
-    int n;
+int n;
+void solve() {
     cin >> n;
-    
-    vector<int> p(n);
-    vector<int> a(n);
-    vector<int> b(n);
-    loop(i, n) {
-        cin >> p[i];
-        cin >> a[i];
-        cin >> b[i];
+    vector<int> p(n), a(n), b(n);
+    for(auto &x: p) {
+        cin >> x;
+    }
+    for(auto &x: a) {
+        cin >> x;
+        x--;
+    }
+    for(auto &x: b) {
+        cin >> x;
+        x--;
+    }
+
+    set<pair<int, int>> st[3];
+
+    for(int i = 0; i < n; i++) {
+        st[a[i]].insert(make_pair(p[i], b[i]));
+        st[b[i]].insert(make_pair(p[i], a[i]));
     }
 
     int m;
     cin >> m;
-    vector<int> c(m);
-    loop(i, m)  cin >> c[i];  
+
+    while(m--) {
+        int x;
+        cin >> x;
+        x--;
+
+        if(st[x].empty()) {
+            cout << -1 << " ";
+            continue;
+        }
+
+        int ans = st[x].begin() -> first;
+        cout << ans << " "; 
+
+        int b = st[x].begin() -> second;
+        st[x].erase({ans, b});
+        st[b].erase({ans, x});
+    }
+}
+
+int main() {
+    NFS
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
     
+    return 0;
 }

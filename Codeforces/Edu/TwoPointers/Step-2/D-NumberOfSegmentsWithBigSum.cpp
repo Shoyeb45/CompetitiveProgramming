20fde8b3fa82e2fa1fc4e539250edf2f1db6a454
Shoyeb45 <shoyebff45@gmail.com>
2024-10-12 20:20:31 +0530

    Number of Segments with Big Sum

---
+++ Codeforces/Edu/TwoPointers/Step-2/D-NumberOfSegmentsWithBigSum.cpp
@@ -0,0 +1,48 @@
+    #include<bits/stdc++.h>
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+template<typename T> void debug(string v1, T t1) { cout << v1 << ": " << t1 << "\n"; }
+template<typename T, typename K> void debug(string v1, T t1, string v2, K t2) { cout << v1 << ": " << t1 << " " << v2 << ": " << t2 << "\n"; }
+const ll mod = 1000000007;
+
+int n;
+ll s;
+void solve() {
+    cin >> n >> s;
+    vector<ll> a(n);
+    for(auto &x: a) {
+        cin >> x;
+    }
+
+    int left = 0;
+    ll ans = 0, currSum = 0;
+    for(int right = 0; right < n; right++) {
+        currSum +=a[right];
+        while(left < n && currSum >= s) {
+            ans += (n - right);
+            currSum -= a[left++];
+        }
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    NFS
+    #ifndef ONLINE_JUDGE
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\input.in", "r", stdin);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\output.out", "w", stdout);
+    #endif
+
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+
+    return 0;
+}

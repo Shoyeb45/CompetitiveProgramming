eec88b46ad171e908b3e3ac84e2fb61d2f0ebd8e
Shoyeb45 <shoyebff45@gmail.com>
2024-10-20 19:27:37 +0530

    Div 2: 980

---
+++ Codeforces/Contest/Div-2/980/A.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+template<typename T> void debug(string v1, T t1) { cout << v1 << ": " << t1 << "\n"; }
+template<typename T, typename K> void debug(string v1, T t1, string v2, K t2) { cout << v1 << ": " << t1 << " " << v2 << ": " << t2 << "\n"; }
+const ll mod = 1000000007;
+
+ll a, b;
+void solve() {
+    cin >> a >> b;
+    if(a >= b) {
+        cout << a << "\n";
+    }
+    else {
+        ll x = b - a;
+        if(x > a) {
+            cout << 0 << "\n";
+        }
+        else {
+            cout << (a - x) << "\n";
+        }
+    }
+}
+
+int main() {
+    NFS
+    #ifndef ONLINE_JUDGE
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\input.in", "r", stdin);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\output.out", "w", stdout);
+    #endif
+
+    int tt;
+    tt = 1;
+    cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+
+    return 0;
+}

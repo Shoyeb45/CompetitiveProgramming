9e466a76274866508947443a122228a769378c73
Shoyeb45 <shoyebff45@gmail.com>
2024-10-27 22:55:52 +0530

    Regular Brackett Sequence

---
+++ Codeforces/Practice/1400/RegularBracketSequence.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+template<typename T> void debug(string v1, T t1) { cout << v1 << ": " << t1 << "\n"; }
+template<typename T, typename K> void debug(string v1, T t1, string v2, K t2) { cout << v1 << ": " << t1 << " " << v2 << ": " << t2 << "\n"; }
+const ll mod = 1000000007;
+
+void solve() {
+    string s;
+    cin >> s;
+
+    int extra = 0;
+    stack<char> st;
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == '(') {
+            st.push('(');
+        }
+        else {
+            if(!st.empty() && st.top() == '(') {
+                st.pop();
+            } 
+            else {
+                extra++;
+            }
+        }
+    }
+
+    cout << (int)(s.size() - st.size() - extra) << "\n";
+}
+
+int main() {
+    NFS
+     #ifndef ONLINE_JUDGE
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\input.in", "r", stdin);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\output.out", "w", stdout);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\error.out", "w", stderr);
+    #endif
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+    return 0;
+}

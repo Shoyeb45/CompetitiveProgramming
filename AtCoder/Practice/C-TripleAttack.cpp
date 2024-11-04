aa9ba80e70bf3df7e95f693c0255362a64bab69d
Shoyeb45 <shoyebff45@gmail.com>
2024-11-04 12:35:23 +0530

    Triple Attack

---
+++ AtCoder/Practice/C-TripleAttack.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+template<typename T> void debug(string v1, T t1) { cout << v1 << ": " << t1 << "\n"; }
+template<typename T, typename K> void debug(string v1, T t1, string v2, K t2) { cout << v1 << ": " << t1 << " " << v2 << ": " << t2 << "\n"; }
+const ll mod = 1000000007;
+
+int n;
+vector<ll> a;
+void solve() {
+    cin >> n;
+    a.resize(n);
+    for(auto &x : a) {
+        cin >> x;
+    }
+
+    ll t = 0;
+    for(auto x : a) {
+        
+        int k = x / 5;
+        t += 1LL * k * 3;
+        x -= 1LL * k * 5;
+
+        while(x > 0) {
+            t++;
+            if(t % 3 == 0) {
+                x -= 3;
+            }
+            else {
+                x -= 1;
+            }
+        }
+    }
+    
+    cout << t << "\n";
+}
+
+int main() {
+    NFS
+     #ifndef ONLINE_JUDGE
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\input.in", "r", stdin);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\output.out", "w", stdout);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\error.out", "w", stderr);
+    #endif
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+    return 0;
+}

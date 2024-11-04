9a0a47b455b9992c3fafb8aa89e597f59271020c
Shoyeb45 <shoyebff45@gmail.com>
2024-11-04 09:15:37 +0530

    Count Arithmetic subarrays

---
+++ AtCoder/Practice/C-CountArithmeticSubarrays.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+template<typename T> void debug(string v1, T t1) { cout << v1 << ": " << t1 << "\n"; }
+template<typename T, typename K> void debug(string v1, T t1, string v2, K t2) { cout << v1 << ": " << t1 << " " << v2 << ": " << t2 << "\n"; }
+const ll mod = 1000000007;
+
+int n;
+vector<ll> a;
+
+void solve() {
+    cin >> n;
+    a.resize(n);
+    for(auto &x : a) {
+        cin >> x;
+    }    
+
+    ll d = 0;
+    ll ans = 1;
+    for(int right = 1, left = 0; right < n; right++) {
+        d = a[right] - a[right - 1];
+
+        while(left + 1 < n && a[left + 1] - a[left] != d) {
+            left++;
+        }
+        
+        ans += right - left + 1;
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    NFS
+     #ifndef ONLINE_JUDGE
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\input.in", "r", stdin);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\output.out", "w", stdout);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\error.out", "w", stderr);
+    #endif
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+    return 0;
+}

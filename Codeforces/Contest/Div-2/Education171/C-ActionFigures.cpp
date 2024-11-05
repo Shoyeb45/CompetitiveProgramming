f1c7fc530be8a32a171ab087ff2c6216ef79abae
Shoyeb45 <shoyebff45@gmail.com>
2024-11-05 09:41:38 +0530

    Action Figures

---
+++ Codeforces/Contest/Div-2/Education171/C-ActionFigures.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+template<typename T> void debug(string v1, T t1) { cout << v1 << ": " << t1 << "\n"; }
+template<typename T, typename K> void debug(string v1, T t1, string v2, K t2) { cout << v1 << ": " << t1 << " " << v2 << ": " << t2 << "\n"; }
+const ll mod = 1000000007;
+
+int n;
+string s;
+void solve() {
+    cin >> n >> s;
+    ll ans = 0, cnt = 0;
+
+    for(int  i = n - 1; i >= 0; i--) {
+        if(s[i] == '1' && cnt < i) {
+            cnt++;
+            continue;
+        }
+        cnt = max(cnt - 1, 0LL);
+        ans += i + 1;
+    }
+
+    cout << ans << "\n";
+}
+
+int main() {
+    NFS
+     #ifndef ONLINE_JUDGE
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\input.in", "r", stdin);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\output.out", "w", stdout);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\error.out", "w", stderr);
+    #endif
+    int tt;
+    tt = 1;
+    cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+    return 0;
+}

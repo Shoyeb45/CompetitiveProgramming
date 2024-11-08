0859cdaaa07036981a05b090e4cf89889ad94c50
Shoyeb45 <shoyebff45@gmail.com>
2024-11-08 19:19:53 +0530

    Even More Odd Photos

---
+++ USACO/Bronze/IntroductionToGreedyAlgorithms/EvenMoreOddPhotos.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+template<typename T> void debug(string v1, T t1) { cout << v1 << ": " << t1 << "\n"; }
+template<typename T, typename K> void debug(string v1, T t1, string v2, K t2) { cout << v1 << ": " << t1 << " " << v2 << ": " << t2 << "\n"; }
+const ll mod = 1000000007;
+
+int n;
+void solve() {
+    cin >> n;
+
+    int cnt[] = {0, 0};
+    for (int i = 0, x; i < n; i++) {
+        cin >> x;
+        cnt[x % 2]++;
+    }
+    while(cnt[1] > cnt[0]) {
+        cnt[0]++, cnt[1] -= 2;
+    }
+
+    if(cnt[0] > cnt[1] + 1) {
+        cnt[0] = cnt[1] + 1;
+    }
+
+    cout << cnt[0] + cnt[1] << "\n";
+}   
+
+int main() {
+    NFS
+    //  #ifndef ONLINE_JUDGE
+    //     freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\input.in", "r", stdin);
+    //     freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\output.out", "w", stdout);
+    //     freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\error.out", "w", stderr);
+    // #endif
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+    return 0;
+}

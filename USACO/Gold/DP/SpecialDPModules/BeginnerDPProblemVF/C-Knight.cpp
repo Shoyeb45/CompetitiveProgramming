1023c9d3cda768bfff1bb02cbbdb8a0f7da2b551
Shoyeb45 <shoyebff45@gmail.com>
2024-11-09 16:06:42 +0530

    Knight

--- USACO/Gold/DP/SpecialDPModules/BeginnerDPProblemVF/C-Knight.cpp
+++ USACO/Gold/DP/SpecialDPModules/BeginnerDPProblemVF/C-Knight.cpp
@@ -5,13 +5,38 @@ using namespace std;
 typedef long long ll;
 const ll mod = 1000000007;
 
+int n, m;
+
+ll dp[51][51];
+ll f(int i, int j) {
+    if (i >= n || j >= m) {
+        return 0;
+    }
+
+    if (i == n - 1 && j == m - 1) {
+        return 1;
+    }
+
+    if (dp[i][j] != -1) {
+        return dp[i][j];
+    }
+
+    ll ans = f(i + 2, j + 1);
+    ans += f(i + 1, j + 2);
+
+    return dp[i][j] = ans;
+}
+
 void solve() {
-    
+    cin >> n >> m;
+    memset(dp, -1, sizeof(dp));
+    cout << f(0, 0) << "\n";
 }
 
 int main() {
-    freopen("input.in", "r", stdin);
-    freopen("input.out", "w", stdout);
+    freopen("knight.in", "r", stdin);
+    freopen("knight.out", "w", stdout);
+    
     NFS
     int tt;
     tt = 1;

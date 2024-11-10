4b1dfa686bfa25edbee4816b507e269122a92e82
Shoyeb45 <shoyebff45@gmail.com>
2024-11-10 22:32:21 +0530

    AlgoZenith contest

---
+++ Miscellaneous/AlgoZenithICPCContest/A-AliceAndCups.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+template<typename T> void debug(string v1, T t1) { cout << v1 << ": " << t1 << "\n"; }
+template<typename T, typename K> void debug(string v1, T t1, string v2, K t2) { cout << v1 << ": " << t1 << " " << v2 << ": " << t2 << "\n"; }
+const ll mod = 1000000007;
+
+string s;
+void changePos(int &idx, char c) {
+    if (c == 'A') {
+        if (idx == 1) {
+            idx = 2;
+        }
+        else if (idx == 2) {
+            idx = 1;
+        }
+    }
+    else if (c == 'B') {
+        if (idx == 2) {
+            idx = 3;
+        }
+        else if (idx == 3) {
+            idx = 2;
+        }
+    }
+    else {
+        if (idx == 1) {
+            idx = 3;
+        }
+        else if (idx == 3) {
+            idx = 1;
+        }
+    }
+}
+
+void solve() {
+    cin >> s;
+
+    int idx = 1;
+
+    for (char c : s) {
+        changePos(idx, c);
+    }
+    cout << idx << "\n";
+}
+
+int main() {
+    NFS
+    #ifndef ONLINE_JUDGE
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\input.in", "r", stdin);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\output.out", "w", stdout);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\error.out", "w", stderr);
+    #endif
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+    return 0;
+}

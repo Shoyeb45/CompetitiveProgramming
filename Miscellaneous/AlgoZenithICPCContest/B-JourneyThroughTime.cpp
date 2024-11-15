1a3d1f7dbc7004b8079a4da608a33aa026b05538
Shoyeb45 <shoyebff45@gmail.com>
2024-11-15 09:39:06 +0530

    Journey Thourgh Time

--- Miscellaneous/AlgoZenithICPCContest/B-JourneyThroughTime.cpp
+++ Miscellaneous/AlgoZenithICPCContest/B-JourneyThroughTime.cpp
@@ -7,24 +7,41 @@ template<typename T> void debug(string v1, T t1) { cout << v1 << ": " << t1 << "
 template<typename T, typename K> void debug(string v1, T t1, string v2, K t2) { cout << v1 << ": " << t1 << " " << v2 << ": " << t2 << "\n"; }
 const ll mod = 1000000007;
 
+// Nice question 
 int n;
 ll eta;
 
 void solve() {
     cin >> n >> eta;
+
     vector<pair<ll, ll>> a(n);
-    for (auto it : a) {
+    for (auto &it : a) {
         cin >> it.first >> it.second;
     }    
 
     sort(range(a));
 
-    
+    priority_queue<pair<ll, ll>> pq;
+
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        while (!pq.empty() && (a[i].first - pq.top().second) > eta) {  // pop out which does not satisfy the condition 
+            pq.pop();
+        }
+
+        if (!pq.empty()) {
+            ll curr = pq.top().first + a[i].first + a[i].second;  
+            ans = max (ans, curr); 
+        }
+        pq.push({a[i].second - a[i].first, a[i].first});
+    }
+
+    cout << ans << "\n";
 }
 
 int main() {
     NFS
-     #ifndef ONLINE_JUDGE
+    #ifndef ONLINE_JUDGE
         freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\input.in", "r", stdin);
         freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\output.out", "w", stdout);
         freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\error.out", "w", stderr);

43354585bef7ee3436dfecc89c51c5f61f10a0d9
Shoyeb45 <shoyebff45@gmail.com>
2025-01-12 09:38:16 +0530

    Contrast value

---
+++ Codeforces/TLE-31_Sheet/1200/ContrastValue.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+template<typename T> void debug(string v1, T t1) { cout << v1 << ": " << t1 << "\n"; }
+template<typename T, typename K> void debug(string v1, T t1, string v2, K t2) { cout << v1 << ": " << t1 << " " << v2 << ": " << t2 << "\n"; }
+const ll mod = 1000000007;
+
+int n;
+vector<int> a;
+void solve() {
+    cin >> n;
+    a.resize(n);
+
+    for (auto &x : a) {
+        cin >> x;
+    }
+    n = unique(range(a)) - a.begin();
+  
+    int ans = 0;
+    for (int i = 1; i + 1 < n; i++) {
+        if (a[i - 1] > a[i] && a[i] > a[i + 1]) {
+            ans++;
+        }
+        else if (a[i - 1] < a[i] && a[i] < a[i + 1]) {
+            ans++;
+        }
+    }
+    // debug("ans", ans);
+    cout << n - ans << "\n";
+}
+
+int main() {
+    NFS
+     #ifndef ONLINE_JUDGE
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\input.in", "r", stdin);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\output.out", "w", stdout);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\error.out", "w", stderr);
+    #endif
+    int tt;
+    tt = 1;
+    cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+    return 0;
+}

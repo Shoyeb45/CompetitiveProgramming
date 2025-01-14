e258451ee1fee43cf09569f69032bca5f5099413
Shoyeb45 <shoyebff45@gmail.com>
2025-01-14 22:53:58 +0530

    Sequence Transformation

---
+++ Codeforces/Practice/1200/SequenceTransformation.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+template<typename T> void debug(string v1, T t1) { cout << v1 << ": " << t1 << "\n"; }
+template<typename T, typename K> void debug(string v1, T t1, string v2, K t2) { cout << v1 << ": " << t1 << " " << v2 << ": " << t2 << "\n"; }
+const ll mod = 1000000007;
+
+int n;
+vector<int> a;
+void solve() {
+    cin >> n;
+    a.resize(n);
+    for (auto &x: a) {
+        cin >> x;
+    }
+
+    int N = unique(range(a)) - a.begin();
+    if (N == 1) {
+        cout << 0 << "\n";
+        return;
+    }
+
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < N; i++) {
+        mp[a[i]]++;
+    }
+
+    int ans = INT_MAX;
+    for (auto it : mp) {
+        if (it.second == 1) {
+            if (it.first == a[0] || it.first == a[N - 1]) {
+                ans = 1;
+            }
+            else {
+                ans = min(ans, 2);
+            }
+            continue;
+        }
+
+        if (it.first == a[0] && it.first == a[N - 1]) {
+            ans = min(ans, it.second - 1);
+        }
+        else if(it.first == a[0] || it.first == a[N - 1]) {
+            ans = min(ans, it.second );
+        }
+        else {
+            ans = min(ans, it.second + 1);
+        }
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    NFS
+     #ifndef ONLINE_JUDGE
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\input.in", "r", stdin);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\output.out", "w", stdout);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\error.out", "w", stderr);
+    #endif
+    int tt;
+    tt = 1;
+    cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+    return 0;
+}

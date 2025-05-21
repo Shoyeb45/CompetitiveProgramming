3faebd15393deeefbe8c2f00abbeebe3d60121cd
Shoyeb45 <shoyebff45@gmail.com>
2025-05-21 06:53:25 +0530

    Deletive Editing

--- Codeforces/TLE-31_Sheet/900/deletiveEditing.cpp
+++ Codeforces/TLE-31_Sheet/900/deletiveEditing.cpp
@@ -1,60 +1,65 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define loop(i, n) for(int i = 0; i < n; i++)
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
 typedef long long ll;
+template<typename T> void debug(string v1, T t1) { cout << v1 << ": " << t1 << "\n"; }
+template<typename T, typename K> void debug(string v1, T t1, string v2, K t2) { cout << v1 << ": " << t1 << " " << v2 << ": " << t2 << "\n"; }
+const ll mod = 1000000007;
 
-
-map<char,int> occurence(string s) {
-    map<char, int> m;
-    for(char & ch : s)  m[ch]++;
-
-    return m;
-}
-
+string s, t;
 void solve() {
-    string s, t;
     cin >> s >> t;
-    
-    if(s == t) {
+
+    int j = s.size() - 1;
+    vector<bool> is_taken(s.size(), false);
+
+    for (int i = t.size() - 1; i >= 0; i--) {
+        for (; j >= 0; j--) {
+            if (t[i] == s[j]) {
+                for (int k = j + 1; k < s.size(); k++) {
+                    if (s[k] == t[i] && !is_taken[k]) {
+                        cout << "NO\n";
+                        return;
+                    }
+                }
+                is_taken[j--] = true;
+                break;
+            }
+        }
+
+        
+        // t[i] == s[j]
+        
+    }
+
+    string ans = "";
+    for (int i = 0; i < s.size(); i++) {
+        if (is_taken[i]) {
+            ans += s[i]; 
+        }
+    }
+    if (ans == t) {
         cout << "YES\n";
         return;
-    } else {
-        map<char, int> map_s = occurence(s);
-        map<char, int> map_t = occurence(t);
-
-        for(char & a: t) {
-            if(map_s[a] - map_t[a] < 0) {
-                cout << "NO\n";
-                return;
-            }
-        }
-
-        for(char & a: t) {
-            for(int i = 0, j = 1; j <= map_s[a] - map_t[a] && i < s.size(); i++) {
-                if(s[i] == a) {
-                    s.erase(i, 1);
-                    j++;
-                }
-            }
-        }
-        map<char, int> map_sm = occurence(s);
-        for(char & a: t) {
-            if(map_sm[a] - map_t[a] < 0) {
-                cout << "NO\n";
-                return;
-            }
-        }
-
-        cout << "YES\n";
     }
-    
+    cout << "NO\n";
 }
 
 int main() {
-    int t;
-    cin >> t;
-    
-    while(t--) {
+    NFS
+     #ifndef ONLINE_JUDGE
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\input.in", "r", stdin);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\output.out", "w", stdout);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\error.out", "w", stderr);
+    #endif
+    int tt;
+    tt = 1;
+    cin >> tt;
+
+    while (tt--) {
         solve();
     }
+
+    return 0;
 }

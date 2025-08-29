e877946d801e08d71a6dd3bd5569b269cb4db43b
Shoyeb45 <shoyebff45@gmail.com>
2025-08-29 20:29:24 +0530

    added maximum subarray sum code

--- CSES/RangeQueries/SubarraySumQueriesII.cpp
+++ CSES/RangeQueries/SubarraySumQueriesII.cpp
@@ -9,9 +9,9 @@ const ll mod = 1000000007;
 
 class Node {
 public:
-    int maxSubarraySum, maxPrefixSum, maxSuffixSum, sum; 
+    ll maxSubarraySum, maxPrefixSum, maxSuffixSum, sum; 
 
-    Node(int maxSubarraySum, int maxPrefixSum, int maxSuffixSum, int sum) {
+    Node(ll maxSubarraySum, ll maxPrefixSum, ll maxSuffixSum, ll sum) {
         this->maxSubarraySum = maxSubarraySum;
         this->maxPrefixSum = maxPrefixSum;
         this->maxSuffixSum = maxSuffixSum;
@@ -23,35 +23,103 @@ public:
 class SegmentTree {
 public:
     int n;
-    vector<Node> tree;
+    vector<Node*> tree;
 
-    SegmentTree(int n) {
-        this.n = n;
+
+    SegmentTree(vector<ll> &a) {
+        this -> n = a.size();
         tree.resize(4 * n + 1);
+        utilBuild(a, 0, n - 1, 0);
     }
 
-    void build(vector<int> &a) {
-
+    ll query(int l, int r) {
+        Node* res = queryUtil(0, n - 1, 0, l, r);
+        return res -> maxSubarraySum;
     }
+
 private:
-    void utilBuild(vector<int> &a, int l, int r, int i) {
+    void utilBuild(vector<ll> &a, int l, int r, int i) {
         if (l == r) {
-            tree[i].maxPrefixSum = tree[i].maxSubarraySum = tree[i].maxSuffixSum = tree[i].sum = a[l]; 
+            tree[i] = new Node(a[l], a[l], a[l], a[l]);
+            // tree[i] -> maxPrefixSum = tree[i] -> maxSubarraySum = tree[i] -> maxSuffixSum = tree[i] -> sum = a[l]; 
+            return;
         }
 
-        int m = (l + r) / 2;
+        int m = (l + r) / 2, left_idx = (2 * i) + 1, right_idx = (2 * i) + 2;
 
-        utilBuild(a, l, m, 2 * i + 1);
-        utilBuild(a, m + 1, r, 2 * i + 2);
+        utilBuild(a, l, m, left_idx);
+        utilBuild(a, m + 1, r, right_idx);
 
-        Node left = tree[2 * i + 1];
-        Node right = tree[2 * i + 2];
+        Node* left = tree[left_idx];
+        Node* right = tree[right_idx];
 
-        tree[i].maxSubarraySum = max({left.maxSubarraySum, })
+        tree[i] = new Node(0, 0, 0, 0);
+        tree[i] -> maxSubarraySum = max({
+            left -> maxSubarraySum, 
+            right -> maxSubarraySum, 
+            left -> maxSuffixSum + right -> maxPrefixSum
+        });
+        tree[i] -> sum = left -> sum + right -> sum;
+        tree[i] -> maxPrefixSum = max(left -> maxPrefixSum, left -> sum + right -> maxPrefixSum);
+        tree[i] -> maxSuffixSum = max(right -> maxSuffixSum, right -> sum + left -> maxSuffixSum);
     }
-};
-void solve() {
+
+    Node* queryUtil(int seg_l, int seg_r, int tree_i, int q_l, int q_r) {
+        if (q_l <= seg_l && seg_r <= q_r) {
+            return tree[tree_i];
+        } else if (q_r < seg_l || seg_r < q_l) {
+            return nullptr;
+        }
+
+        int m = (seg_l + seg_r) / 2, left_idx = (2 * tree_i) + 1, right_idx = (2 * tree_i) + 2;
+        Node* left = queryUtil(seg_l, m, left_idx, q_l, q_r);
+        Node* right = queryUtil(m + 1, seg_r, right_idx, q_l, q_r);
+
+        if (!left) {
+            return right;
+        }
+
+        if (!right) {
+            return left;
+        }
+        
+
+        Node* res = new Node(0, 0, 0, 0);
+        res -> sum = left->sum + right->sum;
+        res -> maxPrefixSum = max(left -> maxPrefixSum, left -> sum + right -> maxPrefixSum);
+        res -> maxSuffixSum = max(right -> maxSuffixSum, right -> sum + left -> maxSuffixSum);
+        res -> maxSubarraySum = max({
+            left -> maxSubarraySum,
+            right -> maxSubarraySum,
+            left -> maxSuffixSum + right -> maxPrefixSum
+        });
+        return res;
+    }
     
+};
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    vector<ll> a(n);
+    for (auto &x: a) {
+        cin >> x;
+    }    
+    
+    vector<pair<int, int>> queries;
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        queries.push_back({l, r});
+    }
+
+    SegmentTree sg(a);
+
+
+    for (auto que: queries) {
+        ll res = sg.query(que.first - 1, que.second - 1);
+        cout << (res < 0? 0: res) << "\n";
+    }
 }
 
 int main() {

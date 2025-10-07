72e4049a20899bfb0c973d6ecc8056563063f2ef
Shoyeb45 <shoyebff45@gmail.com>
2025-10-07 18:55:34 +0530

    Was able to do only 1 question

---
+++ Codeforces/Contest/Div-2/Education183/A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+const ll mod = 1000000007;
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << (n % 3 == 0? 0 : 3 * (n / 3 + 1) - n) << "\n";
+}
+
+int main() {
+    NFS
+    #ifndef ONLINE_JUDGE
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\input.in", "r", stdin);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\output.out", "w", stdout);
+        freopen("E:\\Code\\CompetitveProgramming\\stdin-stdout-stderr\\error.out", "w", stderr);
+    #endif
+    int tt;
+    tt = 1;
+    cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+    return 0;
+}

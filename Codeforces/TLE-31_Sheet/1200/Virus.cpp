aed9c35b392fb369134205f9bb8a0dab454dc925
Shoyeb45 <shoyebff45@gmail.com>
2025-10-24 14:24:35 +0530

    TLE 1200: Virus

--- Codeforces/TLE-31_Sheet/1200/Virus.cpp
+++ Codeforces/TLE-31_Sheet/1200/Virus.cpp
@@ -6,69 +6,34 @@
 #endif
 using namespace std;
 #define range(x) (x).begin(), (x).end()
+#define rrange(x) (x).rbegin(), (x).rend()
 #define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
 typedef long long ll;
-template <typename T>
-using min_heap = priority_queue<T, vector<T>, greater<T>>;
-template <typename T>
-using max_heap = priority_queue<T>;
 const ll mod = 1000000007;
 
 void solve() {
     int n, m;
     cin >> n >> m;
-    vector<int> infe(m);
-    for (int i = 0; i < m; i++) {
-        cin >> infe[i];
-        infe[i]--;
-    }   
-    sort(range(infe));
-    if (n == m) {
-        cout << n << "\n";
-        return;
+    vector<int> a(m);
+    for (int &x: a) {
+        cin >> x;
     }
-
-    vector<pair<int, int>> vp;
-    for (int i = 0; i < m; i++) {
-        int low = infe[i];
-        int high = infe[i];
-        int k = i;
-        while ((infe[k] + 1) % n == infe[(k + 1) % m]) {
-            high = infe[(k + 1) % m];
-            k = (k + 1) % m;
-        }
-        if (k > i) {
-            i = k;
-        }
-        vp.push_back({low, high});
-    }
-    if (vp.size() > 1 && vp.back().second == vp.front().second) {
-        vp[0].first = vp.back().first;
-        vp.pop_back();
-    }
-    debug(vp);
     vector<int> diff;
-    for (int i = 1; i < vp.size(); i++) {
-        diff.push_back(vp[i].first - vp[i - 1].second - 1);
+    sort(range(a));
+    for (int i = 1; i < m; i++) {
+        diff.push_back(a[i] - a[i - 1] - 1);
     }
+    diff.push_back(n - (a[m - 1] - a[0] + 1));
+    sort(rrange(diff));
 
-    if (!vp.empty()) {
-        if (vp.front().first <= vp.front().second) {
-            diff.push_back(vp.front().first + n - vp.back().second - 1);
-        } else {
-            diff.push_back(vp.front().first - vp.back().second - 1);
-        }
-    }
-   
-    sort(diff.rbegin(), diff.rend());
-    int safe = 0, to_sub = 0;
-    debug(diff);
+    int days = 0;
+    ll safe = 0;
     for (int &x: diff) {
-        x = max(0, x - to_sub);
+        x = max(0, x - 2 * days);
         if (x > 0) {
             safe += max(x - 1, 1);
         }
-        to_sub += 4;
+        days += 2;
     }
     cout << n - safe << "\n";
 }

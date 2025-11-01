298cdf6e082bcc0a28e7692c7c4224d0365bbacc
Shoyeb45 <shoyebff45@gmail.com>
2025-11-01 12:35:02 +0530

    Upsolve: Khba Loves To Sleep

--- Codeforces/Contest/Div-4/1062/E-KhbaLovesToSleep.cpp
+++ Codeforces/Contest/Div-4/1062/E-KhbaLovesToSleep.cpp
@@ -18,18 +18,24 @@ void solve() {
     for (int &x: a) {
         cin >> x;
     }   
+    a.push_back(1e9);
+    a.push_back(-1e9);
     
     sort(range(a));
+    n += 2;
+
     auto check = [&](ll mid) -> bool {
         ll cnt = 0;
+        a[0] = -mid, a[n - 1] = x + mid;
         for (int i = 1; i < n; i++) {
             cnt += max(0LL, (a[i] - mid) - (a[i - 1] + mid) + 1);
         }
+        a[0] = -1e9, a[n - 1] = 1e9;
         return cnt >= k;
     };
-    ll low = 0, high = x;
-    ll ans = -1;
 
+    ll low = 0, high = 1e10;
+    ll ans = -1;
     while (low < high) {
         ll mid = (low + high) >> 1;
         debug(low, high);
@@ -41,7 +47,25 @@ void solve() {
             high = mid;
         }
     }
-    cout << ans << "\n";
+
+    int p = 0;
+    a[0] = -ans, a[n - 1] = ans + x;
+
+    set<int> st;
+    for (int i = 1; i < n; i++) {
+        int low = a[i - 1] + ans, high = a[i] - ans;
+        for (int t = low; t <= high; t++) {
+            if (st.find(t) == st.end()) {
+                cout << t << " ";
+                st.insert(t);
+                p++;
+            } 
+            if (p >= k) {
+                cout << "\n";
+                return;
+            }
+        }
+    }
 }
 
 int main() {

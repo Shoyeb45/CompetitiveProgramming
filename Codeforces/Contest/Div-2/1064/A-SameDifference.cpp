e3863c847dca5f7893942dbf4fa6945d555f9314
Shoyeb45 <shoyebff45@gmail.com>
2025-11-16 22:13:08 +0530

    Div 2 1064: Did 3 questions

---
+++ Codeforces/Contest/Div-2/1064/A-SameDifference.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define rrange(x) (x).rbegin(), (x).rend()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+const ll mod = 1000000007;
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++) {
+        ans += s[i] != s.back();
+    }
+    cout << ans << "\n";
+}   
+
+
+
+int main() {
+    NFS
+    #ifndef ONLINE_JUDGE
+        freopen("/home/shoyeb45/Codes/CompetitiveProgramming/stdin-stdout-stderr/in.txt", "r", stdin);
+        freopen("/home/shoyeb45/Codes/CompetitiveProgramming/stdin-stdout-stderr/out.txt", "w", stdout);
+        freopen("/home/shoyeb45/Codes/CompetitiveProgramming/stdin-stdout-stderr/err.txt", "w", stderr);
+    #endif
+    int tt;
+    tt = 1;
+    cin >> tt;
+
+    for (int i = 1; i <= tt; i++) {
+        solve();
+    }
+
+    return 0;
+}

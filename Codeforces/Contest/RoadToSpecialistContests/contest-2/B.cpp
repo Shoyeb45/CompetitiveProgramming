11b37dec3050bef707476c65973861700811c430
Shoyeb45 <shoyebff45@gmail.com>
2025-11-28 16:44:20 +0530

    Upsolved B

--- Codeforces/Contest/RoadToSpecialistContests/contest-2/B.cpp
+++ Codeforces/Contest/RoadToSpecialistContests/contest-2/B.cpp
@@ -1,9 +1,6 @@
 #include<bits/stdc++.h>
-#ifndef ONLINE_JUDGE
-#include "algo/debug.h"
-#else
-#define debug(...) 42
-#endif
+
+
 using namespace std;
 #define range(x) (x).begin(), (x).end()
 #define rrange(x) (x).rbegin(), (x).rend()
@@ -13,25 +10,14 @@ const ll mod = 1000000007;
 
 void solve() {
     int n;
+    cin >> n;
     string s;
-    cin >> n >> s;
+    cin >> s;
+    s = " " + s;
 
-    vector<int> a;
-    
-    for (int i = 0; i < n; i++) {
-        int j = i;
-        while (j < n && s[j] == s[i]) j++;
-        a.push_back(j - i);
-        i = j - 1;
-    }
+    ll ans = 0;
+    for (int i = 1; i <= n; i++) ans += (s[i] != s[i - 1] ? i: 1);
 
-    
-    ll ans = n;
-    for (int i = 0; i < a.size(); i++) {
-        // if (i - 1 >= 0) ans += a[i - 1];
-        if (i + 1 < a.size()) ans += a[i];
-        if (i + 1 < a.size() && i - 1 >= 0) ans += a[i - 1];
-    }
     cout << ans << "\n";
 }   
 
@@ -40,9 +26,9 @@ void solve() {
 int main() {
     NFS
     #ifndef ONLINE_JUDGE
-        freopen("/home/shoyeb45/Codes/CompetitiveProgramming/stdin-stdout-stderr/in.txt", "r", stdin);
-        freopen("/home/shoyeb45/Codes/CompetitiveProgramming/stdin-stdout-stderr/out.txt", "w", stdout);
-        freopen("/home/shoyeb45/Codes/CompetitiveProgramming/stdin-stdout-stderr/err.txt", "w", stderr);
+        freopen("/Users/keerthi/Codes/CompetitiveProgramming/stdin-stdout-stderr/in.txt", "r", stdin);
+        freopen("/Users/keerthi/Codes/CompetitiveProgramming/stdin-stdout-stderr/out.txt", "w", stdout);
+        freopen("/Users/keerthi/Codes/CompetitiveProgramming/stdin-stdout-stderr/err.txt", "w", stderr);
     #endif
     int tt;
     tt = 1;

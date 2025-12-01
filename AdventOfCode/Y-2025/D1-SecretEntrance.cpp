ae9c2104430c12b938e058e48ffa6b36867767c6
Shoyeb45 <shoyebff45@gmail.com>
2025-12-01 19:59:20 +0530

    Advent of code 2025 Day 1

---
+++ AdventOfCode/Y-2025/D1-SecretEntrance.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define range(x) (x).begin(), (x).end()
+#define rrange(x) (x).rbegin(), (x).rend()
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+const ll mod = 1000000007;
+
+void solve() {
+    ll ans = 0;
+    int pos = 50;
+
+    string inst;
+
+    while ((cin >> inst)) {
+        cout << "old pos: " << pos << " ";
+        int rotate = stoi(inst.substr(1));
+        if (inst[0] == 'L') {
+            // pos = (pos - rotate + 100) % 100;
+            for (int i = 0; i < rotate; i++) {
+                pos = (pos - 1 + 100) % 100;
+                ans += (pos == 0);
+            }
+        } else {
+            for (int i = 0; i < rotate; i++) {
+                pos = (pos + 1) % 100;
+                ans += (pos == 0);
+            }
+        }
+        // ans += (pos == 0);
+        cout << "New pos " << pos << "\n";
+    }
+    cout << ans << "\n";
+}   
+
+
+
+int main() {
+    NFS
+    #ifndef ONLINE_JUDGE
+        freopen("/Users/keerthi/Codes/CompetitiveProgramming/stdin-stdout-stderr/in.txt", "r", stdin);
+        freopen("/Users/keerthi/Codes/CompetitiveProgramming/stdin-stdout-stderr/out.txt", "w", stdout);
+        freopen("/Users/keerthi/Codes/CompetitiveProgramming/stdin-stdout-stderr/err.txt", "w", stderr);
+    #endif
+    int tt;
+    tt = 1;
+    // cin >> tt;
+
+    for (int i = 1; i <= tt; i++) {
+        solve();
+    }
+
+    return 0;
+}

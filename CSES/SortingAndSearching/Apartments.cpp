83f2503a593bfbd9bca670c2272c51820ca7af21
Shoyeb45 <shoyebff45@gmail.com>
2025-12-13 15:15:09 +0530

    CSES Sorting And Searching: Apartments

--- CSES/SortingAndSearching/Apartments.cpp
+++ CSES/SortingAndSearching/Apartments.cpp
@@ -12,7 +12,54 @@ typedef long long ll;
 const ll mod = 1000000007;
 
 void solve() {
+    int n, m, k;
+    cin >> n >> m >> k;
 
+    vector<int> a(n);
+    multiset<int> apartments;
+
+    for (int& i: a) cin >> i;
+    
+    for (int i = 0; i < m; i++) {
+        int x;
+        cin >> x;
+        apartments.insert(x);
+    }
+
+    ll ans = 0;
+
+    sort(range(a));
+
+    for (auto x: a) {
+        if (apartments.empty()) break;
+
+        int low = x - k, high = x + k;
+
+        auto low_it = apartments.lower_bound(low);
+        auto high_it = apartments.upper_bound(high);
+
+        vector<int> to_del;
+        if (high_it != apartments.begin())
+            high_it = prev(high_it);
+
+        int cnt = 0;
+
+        for (auto it = low_it; it != next(high_it); it = next(it)) {
+            if (*it >= low && *it <= high) {
+                to_del.push_back(*it);
+                cnt++;
+                break;
+            }
+        }
+        if (cnt > 0) 
+            ans++;
+
+        for (auto y: to_del) {
+            apartments.erase(apartments.find(y));
+        }
+    }
+    
+    cout << ans << "\n";
 }   
 
 
